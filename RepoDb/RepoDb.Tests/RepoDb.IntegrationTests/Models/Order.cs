f250a37cc46d8a0c89b0972b32d243cec566e3ba
Michael Pendon <michael.c.pendon@outlook.com>
2018-07-15 10:47:29 +0800

    Added RepoDb.IntegrationTests

---
+++ RepoDb/RepoDb.Tests/RepoDb.IntegrationTests/Models/Order.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using RepoDb.Attributes;
+using RepoDb.Enumerations;
+
+namespace RepoDb.IntegrationTests.Models
+{
+    [Map("[dbo].[Order]")]
+    public class Order : DataEntity
+    {
+        [Attributes.Ignore(Command.Insert | Command.Update | Command.Merge | Command.InlineUpdate), Map("Id")]
+        public int Id { get; set; }
+
+        public Guid GlobalId { get; set; }
+
+        public DateTime OrderDateUtc { get; set; }
+
+        public long CustomerId { get; set; }
+
+        public decimal SubTotal{ get; set; }
+
+        public decimal Freight { get; set; }
+
+        public decimal Tax { get; set; }
+
+        public decimal TotalDue { get; set; }
+
+        public DateTime LastUpdatedUtc { get; set; }
+
+        [Attributes.Ignore(Command.Update)]
+        public DateTime DateInsertedUtc { get; set; }
+
+        public string LastUserId { get; set; }
+    }
+}
--- RepoDb/RepoDb/TypeMapper.cs
+++ RepoDb/RepoDb/TypeMapper.cs
@@ -30,7 +30,18 @@ namespace RepoDb
         /// <param name="dbType">The database type where to map the .NET CLR Type.</param>
         public static void AddMap(Type type, DbType dbType)
         {
-            AddMap(new TypeMap(type, dbType));
+            AddMap(type, dbType, false);
+        }
+
+        /// <summary>
+        /// Adds a mapping between .NET CLR Type and database type.
+        /// </summary>
+        /// <param name="type">The .NET CLR Type to be mapped.</param>
+        /// <param name="dbType">The database type where to map the .NET CLR Type.</param>
+        /// <param name="force">A value that indicates whether to force the mapping. If one is already exists, then it will be overwritten.</param>
+        public static void AddMap(Type type, DbType dbType, bool force = false)
+        {
+            AddMap(new TypeMap(type, dbType), force);
         }
 
         /// <summary>
@@ -39,8 +50,18 @@ namespace RepoDb
         /// <param name="typeMap">The instance of type-mapping object that holds the mapping of .NET CLR Type and database type.</param>
         public static void AddMap(TypeMap typeMap)
         {
+            AddMap(typeMap, false);
+        }
+
+        /// <summary>
+        /// Adds a mapping between .NET CLR Type and database type.
+        /// </summary>
+        /// <param name="typeMap">The instance of type-mapping object that holds the mapping of .NET CLR Type and database type.</param>
+        /// <param name="force">A value that indicates whether to force the mapping. If one is already exists, then it will be overwritten.</param>
+        public static void AddMap(TypeMap typeMap, bool force = false)
+        {
             var target = Get(typeMap.Type);
-            if (target != null)
+            if (target != null && force == false)
             {
                 throw new DuplicateTypeMapException($"A mapping for type '{target.Type.FullName}' is already defined. It is currently mapped to '{target.DbType.GetType().FullName}' database type.");
             }
