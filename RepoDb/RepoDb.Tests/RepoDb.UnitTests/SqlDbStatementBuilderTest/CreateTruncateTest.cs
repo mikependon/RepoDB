b985d538d7d47b462cd2187951e2dd2f0a586081
Michael Pendon <michael.c.pendon@outlook.com>
2018-07-19 22:14:21 +0800

    UnitTest for CreateTruncate

---
+++ RepoDb/RepoDb.Tests/RepoDb.UnitTests/SqlDbStatementBuilderTest/CreateTruncateTest.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using RepoDb.Attributes;
+
+namespace RepoDb.UnitTests.SqlDbStatementBuilderTest
+{
+    [TestFixture]
+    public class CreateTruncateTest
+    {
+        private class TestCreateCountWithoutMappingsClass : DataEntity
+        {
+        }
+
+        [Test]
+        public void TestCreateCountWithoutMappings()
+        {
+            // Setup
+            var statementBuilder = new SqlDbStatementBuilder();
+            var queryBuilder = new QueryBuilder<TestCreateCountWithoutMappingsClass>();
+
+            // Act
+            var actual = statementBuilder.CreateTruncate(queryBuilder);
+            var expected = $"" +
+                $"TRUNCATE TABLE [TestCreateCountWithoutMappingsClass] ;";
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Map("ClassName")]
+        private class TestCreateCountWitClassMappingsClass : DataEntity
+        {
+        }
+
+        [Test]
+        public void TestCreateCountWitClassMappings()
+        {
+            // Setup
+            var statementBuilder = new SqlDbStatementBuilder();
+            var queryBuilder = new QueryBuilder<TestCreateCountWitClassMappingsClass>();
+
+            // Act
+            var actual = statementBuilder.CreateTruncate(queryBuilder);
+            var expected = $"" +
+                $"TRUNCATE TABLE [ClassName] ;";
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
