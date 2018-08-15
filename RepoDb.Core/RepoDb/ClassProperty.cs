f633095ccd8cb2841e73a86c35d81b945fd25f37
Michael Pendon <michael.c.pendon@outlook.com>
2018-08-16 03:49:03 +0800

    Performance: Introduced an advance ClassProperty object to manage the reflection states

--- RepoDb.Core/RepoDb/ClassProperty.cs
+++ RepoDb.Core/RepoDb/ClassProperty.cs
@@ -1,4 +1,5 @@
 ﻿using RepoDb.Attributes;
+using RepoDb.Enumerations;
 using RepoDb.Extensions;
 using System;
 using System.Data;
@@ -11,16 +12,6 @@ namespace RepoDb
     /// </summary>
     public class ClassProperty : IEquatable<ClassProperty>
     {
-        #region Privates
-
-        private bool? m_isIdentity;
-        private bool? m_isPrimary;
-        private bool m_isDbTypeWasSet;
-        private DbType? m_dbType;
-        private string m_mappedName;
-
-        #endregion
-
         /// <summary>
         /// Creates a new instance of <i>ClassProperty</i> object.
         /// </summary>
@@ -44,6 +35,72 @@ namespace RepoDb
 
         #region Methods
 
+        /*
+         * GetPrimaryAttribute
+         */
+
+        private bool m_isPrimaryAttributeWasSet;
+        private PrimaryAttribute m_primaryAttribute;
+
+        /// <summary>
+        /// Gets the primary attribute if present.
+        /// </summary>
+        /// <returns>The primary attribute if present.</returns>
+        public PrimaryAttribute GetPrimaryAttribute()
+        {
+            if (m_isPrimaryAttributeWasSet)
+            {
+                return m_primaryAttribute;
+            }
+            m_isPrimaryAttributeWasSet = true;
+            return m_primaryAttribute = Property.GetCustomAttribute(typeof(PrimaryAttribute)) as PrimaryAttribute;
+        }
+
+        /*
+         * GetIdentityAttribute
+         */
+        private bool m_isIdentityAttributeWasSet;
+        private IdentityAttribute m_identityAttribute;
+
+        /// <summary>
+        /// Gets the identity attribute if present.
+        /// </summary>
+        /// <returns>The identity attribute if present.</returns>
+        public IdentityAttribute GetIdentityAttribute()
+        {
+            if (m_isIdentityAttributeWasSet)
+            {
+                return m_identityAttribute;
+            }
+            m_isIdentityAttributeWasSet = true;
+            return m_identityAttribute = Property.GetCustomAttribute(typeof(IdentityAttribute)) as IdentityAttribute;
+        }
+
+        /*
+         * GetIgnoreAttribute
+         */
+        private bool m_isIgnoreAttributeWasSet;
+        private IgnoreAttribute m_ignoreAttribute;
+
+        /// <summary>
+        /// Gets the ignore attribute if present.
+        /// </summary>
+        /// <returns>The ignore attribute if present.</returns>
+        public IgnoreAttribute GetIgnoreAttribute()
+        {
+            if (m_isIgnoreAttributeWasSet)
+            {
+                return m_ignoreAttribute;
+            }
+            m_isIgnoreAttributeWasSet = true;
+            return m_ignoreAttribute = Property.GetCustomAttribute(typeof(IgnoreAttribute)) as IgnoreAttribute;
+        }
+
+        /*
+         * IsPrimary
+         */
+        private bool? m_isPrimary;
+
         /// <summary>
         /// Gets a value whether the current property is a primary property.
         /// </summary>
@@ -54,8 +111,14 @@ namespace RepoDb
             {
                 return m_isPrimary;
             }
-            return m_isPrimary = (Property.GetCustomAttribute(typeof(PrimaryAttribute)) != null);
+            return m_isPrimary = (GetPrimaryAttribute() != null);
         }
+
+        /*
+         * IsIdentity
+         */
+
+        private bool? m_isIdentity;
 
         /// <summary>
         /// Gets a value whether the current property is an identity or not.
@@ -67,8 +130,14 @@ namespace RepoDb
             {
                 return m_isIdentity;
             }
-            return m_isIdentity = (Property.GetCustomAttribute(typeof(IdentityAttribute)) != null);
+            return m_isIdentity = (GetIdentityAttribute() != null);
         }
+
+        /*
+         * GetDbType
+         */
+        private bool m_isDbTypeWasSet;
+        private DbType? m_dbType;
 
         /// <summary>
         /// Gets the mapped <i>DbType</i> for the current property.
@@ -87,6 +156,12 @@ namespace RepoDb
                 TypeMapper.Get(GetUnderlyingType(Property.PropertyType))?.DbType;
         }
 
+        /*
+         * GetMappedName
+         */
+
+        private string m_mappedName;
+
         /// <summary>
         /// Gets the mapped-name for the current property.
         /// </summary>
@@ -100,6 +175,10 @@ namespace RepoDb
             return m_mappedName = ClassExpression.GetPropertyMappedName(Property);
         }
 
+        /*
+         * GetUnderlyingType
+         */
+
         /// <summary>
         /// Gets the underlying type if present.
         /// </summary>
@@ -108,6 +187,273 @@ namespace RepoDb
         private static Type GetUnderlyingType(Type type)
         {
             return type != null ? Nullable.GetUnderlyingType(type) ?? type : null;
+        }
+
+        /*
+         * IsIgnored
+         */
+
+        /// <summary>
+        /// Identifies whether the current property is ignored based on the command.
+        /// </summary>
+        /// <param name="command">The target command.</param>
+        /// <returns>Returns true if the property has been ignored based on the target command.</returns>
+        private bool IsIgnored(Command command)
+        {
+            var ignoreAttribute = GetIgnoreAttribute();
+            if (ignoreAttribute != null)
+            {
+                return (ignoreAttribute.Command & command) == command &&
+                    (ignoreAttribute.Command != Command.None);
+            }
+            return false;
+        }
+
+        /*
+         * IsQueryable
+         */
+
+        private bool? m_isQueryable;
+
+        /// <summary>
+        /// Gets the value whether this property is queryable.
+        /// </summary>
+        /// <returns>The value that defines whether this property is queryable.</returns>
+        public bool? IsQueryable()
+        {
+            if (m_isQueryable != null)
+            {
+                return m_isQueryable;
+            }
+            return m_isQueryable = IsIgnored(Command.Query) == false;
+        }
+
+        /*
+         * IsInsertable
+         */
+
+        private bool? m_isInsertable;
+
+        /// <summary>
+        /// Gets the value whether this property is insertable.
+        /// </summary>
+        /// <returns>The value that defines whether this property is insertable.</returns>
+        public bool? IsInsertable()
+        {
+            if (m_isInsertable != null)
+            {
+                return m_isInsertable;
+            }
+            return m_isInsertable = IsIgnored(Command.Insert) == false;
+        }
+
+        /*
+         * IsUpdateable
+         */
+
+        private bool? m_isUpdateable;
+
+        /// <summary>
+        /// Gets the value whether this property is updateable.
+        /// </summary>
+        /// <returns>The value that defines whether this property is updateable.</returns>
+        public bool? IsUpdateable()
+        {
+            if (m_isUpdateable != null)
+            {
+                return m_isUpdateable;
+            }
+            return m_isUpdateable = IsIgnored(Command.Update) == false;
+        }
+
+        /*
+         * IsDeletable
+         */
+
+        private bool? m_isDeletable;
+
+        /// <summary>
+        /// Gets the value whether this property is deletable.
+        /// </summary>
+        /// <returns>The value that defines whether this property is deletable.</returns>
+        public bool? IsDeletable()
+        {
+            if (m_isDeletable != null)
+            {
+                return m_isDeletable;
+            }
+            return m_isDeletable = IsIgnored(Command.Delete) == false;
+        }
+
+        /*
+         * IsMergeable
+         */
+
+        private bool? m_isMergeable;
+
+        /// <summary>
+        /// Gets the value whether this property is mergeable.
+        /// </summary>
+        /// <returns>The value that defines whether this property is mergeable.</returns>
+        public bool? IsMergeable()
+        {
+            if (m_isMergeable != null)
+            {
+                return m_isMergeable;
+            }
+            return m_isMergeable = IsIgnored(Command.Merge) == false;
+        }
+
+        /*
+         * IsBatchQueryable
+         */
+
+        private bool? m_isBatchQueryable;
+
+        /// <summary>
+        /// Gets the value whether this property is batch-queryable.
+        /// </summary>
+        /// <returns>The value that defines whether this property is batch-queryable.</returns>
+        public bool? IsBatchQueryable()
+        {
+            if (m_isBatchQueryable != null)
+            {
+                return m_isBatchQueryable;
+            }
+            return m_isBatchQueryable = IsIgnored(Command.BatchQuery) == false;
+        }
+
+        /*
+         * IsBatchQueryable
+         */
+
+        private bool? m_isInlineUpdateable;
+
+        /// <summary>
+        /// Gets the value whether this property is inline-updateable.
+        /// </summary>
+        /// <returns>The value that defines whether this property is inline-updateable.</returns>
+        public bool? IsInlineUpdateable()
+        {
+            if (m_isInlineUpdateable != null)
+            {
+                return m_isInlineUpdateable;
+            }
+            return m_isInlineUpdateable = IsIgnored(Command.InlineUpdate) == false;
+        }
+
+        /*
+         * IsInlineMergeable
+         */
+
+        private bool? m_isInlineMergeable;
+
+        /// <summary>
+        /// Gets the value whether this property is inline-merge.
+        /// </summary>
+        /// <returns>The value that defines whether this property is inline-merge.</returns>
+        public bool? IsInlineMergeable()
+        {
+            if (m_isInlineMergeable != null)
+            {
+                return m_isInlineMergeable;
+            }
+            return m_isInlineMergeable = IsIgnored(Command.InlineMerge) == false;
+        }
+
+        /*
+         * IsInlineInsertable
+         */
+
+        private bool? m_isInlineInsertable;
+
+        /// <summary>
+        /// Gets the value whether this property is inline-insert.
+        /// </summary>
+        /// <returns>The value that defines whether this property is inline-insert.</returns>
+        public bool? IsInlineInsertable()
+        {
+            if (m_isInlineInsertable != null)
+            {
+                return m_isInlineInsertable;
+            }
+            return m_isInlineInsertable = IsIgnored(Command.InlineInsert) == false;
+        }
+
+        /*
+         * IsBulkInsertable
+         */
+
+        private bool? m_isBulkInsertable;
+
+        /// <summary>
+        /// Gets the value whether this property is bulk-insertable.
+        /// </summary>
+        /// <returns>The value that defines whether this property is bulk-insertable.</returns>
+        public bool? IsBulkInsertable()
+        {
+            if (m_isBulkInsertable != null)
+            {
+                return m_isBulkInsertable;
+            }
+            return m_isBulkInsertable = IsIgnored(Command.BulkInsert) == false;
+        }
+
+        /*
+         * IsCountable
+         */
+
+        private bool? m_isCountable;
+
+        /// <summary>
+        /// Gets the value whether this property is countable.
+        /// </summary>
+        /// <returns>The value that defines whether this property is countable.</returns>
+        public bool? IsCountable()
+        {
+            if (m_isCountable != null)
+            {
+                return m_isCountable;
+            }
+            return m_isCountable = IsIgnored(Command.Count) == false;
+        }
+
+        /*
+         * IsDeletableAll
+         */
+
+        private bool? m_isDeletableAll;
+
+        /// <summary>
+        /// Gets the value whether this property is deletable (all).
+        /// </summary>
+        /// <returns>The value that defines whether this property is deletable (all).</returns>
+        public bool? IsDeletableAll()
+        {
+            if (m_isDeletableAll != null)
+            {
+                return m_isDeletableAll;
+            }
+            return m_isDeletableAll = IsIgnored(Command.DeleteAll) == false;
+        }
+
+        /*
+         * IsTruncatable
+         */
+
+        private bool? m_isTruncatable;
+
+        /// <summary>
+        /// Gets the value whether this property is truncatable.
+        /// </summary>
+        /// <returns>The value that defines whether this property is truncatable.</returns>
+        public bool? IsTruncatable()
+        {
+            if (m_isTruncatable != null)
+            {
+                return m_isTruncatable;
+            }
+            return m_isTruncatable = IsIgnored(Command.Truncate) == false;
         }
 
         #endregion
