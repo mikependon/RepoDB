0ec145ff9bd7402dc6305ce1b4d5b16c7347cf34
Michael Pendon <michael.c.pendon@outlook.com>
2019-02-05 23:09:15 +0100

    Updated the build.ps1

--- RepoDb.Nuget/build.ps1
+++ RepoDb.Nuget/build.ps1
@@ -21,6 +21,12 @@ Write-Host "RepoDb (NETStandard):`t" + $repoDbStandardLocation
 ## Set the 'MSBuild' exe location
 $msBuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
 
+## Delete the 'bin' folder for (NETFramework)
+$binPath = $repoDbLocation + "\RepoDb\bin"
+if ([System.IO.Directory]::Exists($binPath)) {
+    Remove-Item $binPath -Force -Verbose -Recurse
+}
+
 ## Build the (NETFramework)
 $filename = $repoDbLocation + "\RepoDb.sln"
 Write-Host ""
@@ -31,8 +37,21 @@ Write-Host ""
 Write-Host ""
 Write-Host "Build completed for RepoDb (NETFramework) solution" -ForegroundColor Green
 
+## Verify the 'bin' folder for (NETFramework)
+if (![System.IO.Directory]::Exists($binPath + "\Release")) {
+    Write-Host "Build failed for RepoDb (NETFramework) solution. The 'bin\Release' folder is not found." -ForegroundColor Red
+    return
+}
+
+## Delete the 'bin' folder for (NETStandard)
+$binPath = $repoDbStandardLocation + "\RepoDb\bin"
+if ([System.IO.Directory]::Exists($binPath)) {
+    Remove-Item $binPath -Force -Verbose -Recurse
+}
+
 ## Build the (NETStandard)
 $filename = $repoDbStandardLocation + "\RepoDb.Core.sln"
+Write-Host $filename 
 Write-Host ""
 Write-Host "----------------------------------------"
 Write-Host "Building the RepoDb (NETStandard) solution"
@@ -40,6 +59,12 @@ Write-Host ""
 & dotnet msbuild $filename "/p:configuration=Release" "/t:Clean,Build"
 Write-Host ""
 Write-Host "Build completed for RepoDb (NETStandard) solution" -ForegroundColor Green
+
+## Verify the 'bin' folder for (NETStandard)
+if (![System.IO.Directory]::Exists($binPath + "\Release")) {
+    Write-Host "Build failed for RepoDb (NETStandard) solution. The 'bin\Release' folder is not found." -ForegroundColor Red
+    return
+}
 
 ## Copy the files for 'NETFramework'
 $path = $repoDbLocation + "\RepoDb\bin\Release"
@@ -53,6 +78,12 @@ Copy-item -Path ($path + "\RepoDb.dll") -Destination ($dropoff + "\RepoDb.dll") 
 Copy-item -Path ($path + "\RepoDb.xml") -Destination ($dropoff + "\RepoDb.xml") -Force -Recurse -Verbose
 Write-Host "Files for (NETFramework) has been copied to dropoff path." -ForegroundColor Green
 
+## Verify the copied files for (NETFramework)
+if (( Get-ChildItem $dropoff | Measure-Object).Count -lt 1) {
+    Write-Host "Build failed for RepoDb (NETFramework) solution. There are no files found at dropoff folder." -ForegroundColor Red
+    return
+}
+
 ## Copy the files for 'NETStandard'
 $path = $repoDbStandardLocation + "\RepoDb\bin\Release\netstandard1.5"
 $dropoff = $currentLocation + "\lib\netstandard1.3"
@@ -64,6 +95,12 @@ Write-Host "To:`t`t" + $dropoff
 Copy-item -Path ($path + "\RepoDb.dll") -Destination ($dropoff + "\RepoDb.dll") -Force -Recurse -Verbose
 Copy-item -Path ($path + "\RepoDb.xml") -Destination ($dropoff + "\RepoDb.xml") -Force -Recurse -Verbose
 Write-Host "Files for (NETStandard) has been copied to dropoff path." -ForegroundColor Green
+
+## Verify the copied files for (NETStandard)
+if (( Get-ChildItem $dropoff | Measure-Object).Count -lt 1) {
+    Write-Host "Build failed for RepoDb (NETStandard) solution. There are no files found at dropoff folder." -ForegroundColor Red
+    return
+}
 
 ## Execute the 'Nuget' pack
 $nuget = $currentLocation + "\nuget.exe"
