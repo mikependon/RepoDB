5bb58cf619a4a771904e0085006f67a757dfe04d
Michael Pendon <michael.c.pendon@outlook.com>
2019-03-02 14:22:44 +0100

    Renamed the IntegrationTest Files for QueryGroup

---
+++ RepoDb/RepoDb.Tests/RepoDb.IntegrationTests/Setup/Database.cs
@@ -0,0 +1,105 @@
+﻿using System.Data.SqlClient;
+
+namespace RepoDb.IntegrationTests.Setup
+{
+    /// <summary>
+    /// A class used to setup test database for RepoDb.
+    /// </summary>
+    public static class Database
+    {
+        private static readonly string m_connectionStringForMaster = @"Server=.;Database=master;Integrated Security=True;";
+        private static readonly string m_connectionStringForRepoDb = @"Server=.;Database=RepoDbTestDb;Integrated Security=True;";
+
+        /// <summary>
+        /// Initialize the creation of the database.
+        /// </summary>
+        public static void Init()
+        {
+            // Create the database first
+            CreateDatabase();
+        }
+
+        /// <summary>
+        /// Creates a test database for RepoDb.
+        /// </summary>
+        public static void CreateDatabase()
+        {
+            var commandText = @"IF (NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'RepoDbTestDb'))
+                BEGIN
+	                CREATE DATABASE [RepoDbTestDb];
+                END";
+            using (var connection = new SqlConnection(m_connectionStringForMaster).EnsureOpen())
+            {
+                connection.ExecuteNonQuery(commandText);
+            }
+        }
+
+        /// <summary>
+        /// Create the necessary tables for testing.
+        /// </summary>
+        public static void CreateTables()
+        {
+            CreateCompleteTable();
+        }
+
+        #region CreateTables
+
+        /// <summary>
+        /// Creates a table that has a complete fields. All fields are nullable.
+        /// </summary>
+        public static void CreateCompleteTable()
+        {
+            var commandText = @"IF (NOT EXISTS(SELECT 1 FROM [sys].[objects] WHERE type = 'U' AND name = 'CompleteTable'))
+                BEGIN
+	                CREATE TABLE [dbo].[CompleteTable]
+	                (
+		                [SessionId] UNIQUEIDENTIFIER NOT NULL,
+		                [ColumnBigInt] BIGINT NULL,
+		                [ColumnBinary] BINARY(4000) NULL,
+		                [ColumnBit] BIT NULL,
+		                [ColumnChar] CHAR(32) NULL,
+		                [ColumnDate] DATE NULL,
+		                [ColumnDateTime] DATETIME NULL,
+		                [ColumnDateTime2] DATETIME2(7) NULL,
+		                [ColumnDateTimeOffset] DATETIMEOFFSET(7) NULL,
+		                [ColumnDecimal] DECIMAL(18, 4) NULL,
+		                [ColumnFloat] FLOAT NULL,
+		                [ColumnGeography] GEOGRAPHY NULL,
+		                [ColumnGeometry] GEOMETRY NULL,
+		                [ColumnHierarchyid] HIERARCHYID NULL,
+		                [ColumnImage] IMAGE NULL,
+		                [ColumnInt] INT NULL,
+		                [ColumnMoney] MONEY NULL,
+		                [ColumnNChar] NCHAR(32) NULL,
+		                [ColumnNText] NTEXT NULL,
+		                [ColumnNumeric] NUMERIC(18, 4) NULL,
+		                [ColumnNVarChar] NVARCHAR(MAX) NULL,
+		                [ColumnReal] REAL NULL,
+		                [ColumnSmallDateTime] SMALLDATETIME NULL,
+		                [ColumnSmallInt] SMALLINT NULL,
+		                [ColumnSmallMoney] SMALLMONEY NULL,
+		                [ColumnSqlVariant] SQL_VARIANT NULL,
+		                [ColumnText] TEXT NULL,
+		                [ColumnTime] TIME(7) NULL,
+		                [ColumnTimestamp] TIMESTAMP NULL,
+		                [ColumnTinyInt] TINYINT NULL,
+		                [ColumnUniqueIdentifier] UNIQUEIDENTIFIER NULL,
+		                [ColumnVarBinary] VARBINARY(MAX) NULL,
+		                [ColumnVarchar] VARCHAR(MAX) NULL,
+		                [ColumnXml] XML NULL,
+		                CONSTRAINT [SessionId] PRIMARY KEY 
+		                (
+			                [SessionId] ASC
+		                )
+	                ) ON [PRIMARY];
+	                ALTER TABLE [dbo].[CompleteTable] ADD CONSTRAINT [DF_CompleteTable_SessionId] DEFAULT (NEWID()) FOR [SessionId];
+                END";
+            using (var connection = new SqlConnection(m_connectionStringForMaster).EnsureOpen())
+            {
+                connection.ExecuteNonQuery(commandText);
+            }
+        }
+
+        #endregion
+    }
+}
