a1de6b073f5dd39fa4e5b5300e8e75214e2d9bc7
Michael Pendon <michael.c.pendon@outlook.com>
2019-04-13 19:18:05 +0200

    Added the new classes for the Integration Tests

---
+++ RepoDb/RepoDb.Tests/RepoDb.IntegrationTests/Models/IdentityTable.cs
@@ -0,0 +1,28 @@
+﻿using RepoDb.Attributes;
+using System;
+
+namespace RepoDb.IntegrationTests.Models
+{
+    public class IdentityTable
+    {
+        public long Id { get; set; }
+        public Guid RowGuid { get; set; }
+        public bool? ColumnBit { get; set; }
+        public DateTime? ColumnDateTime { get; set; }
+        public DateTime? ColumnDateTime2 { get; set; }
+        public decimal? ColumnDecimal { get; set; }
+        public float? ColumnFloat { get; set; }
+        public int? ColumnInt { get; set; }
+        public string ColumnNVarChar { get; set; }
+    }
+
+    [Map("[dbo].[IdentityTable]")]
+    public class LiteIdentityTable
+    {
+        public long Id { get; set; }
+        public Guid RowGuid { get; set; }
+        public bool? ColumnBit { get; set; }
+        public DateTime? ColumnDateTime { get; set; }
+        public int? ColumnInt { get; set; }
+    }
+}
---
+++ RepoDb/RepoDb.Tests/RepoDb.IntegrationTests/Models/NonIdentityTable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RepoDb.IntegrationTests.Models
+{
+    public class NonIdentityTable
+    {
+        public Guid Id { get; set; }
+        public bool? ColumnBit { get; set; }
+        public DateTime? ColumnDateTime { get; set; }
+        public DateTime? ColumnDateTime2 { get; set; }
+        public decimal? ColumnDecimal { get; set; }
+        public float? ColumnFloat { get; set; }
+        public int? ColumnInt { get; set; }
+        public string ColumnNVarChar { get; set; }
+    }
+}
