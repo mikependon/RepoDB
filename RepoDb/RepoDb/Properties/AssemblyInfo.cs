f484f061c75cb9b98e0b6e2780a376543d2fe6f7
Michael Pendon <michael.c.pendon@outlook.com>
2019-09-18 23:47:07 +0200

    Nuget Package v1.9.9-beta1

--- RepoDb/RepoDb/Properties/AssemblyInfo.cs
+++ RepoDb/RepoDb/Properties/AssemblyInfo.cs
@@ -35,7 +35,7 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.9.8.4")]
-[assembly: AssemblyFileVersion("1.9.8.4")]
+[assembly: AssemblyVersion("1.9.9.1")]
+[assembly: AssemblyFileVersion("1.9.9.1")]
 [assembly: NeutralResourcesLanguage("")]
 
