05563a27a83482b60e1262d4abfe1eea180690b7
Michael Pendon <michael.c.pendon@outlook.com>
2019-10-26 14:48:45 +0200

    Adhoc: Create an extension method that converts the System.Array to IEnumerable<T> #311

--- RepoDb.Core/RepoDb/Extensions/ArrayExtension.cs
+++ RepoDb.Core/RepoDb/Extensions/ArrayExtension.cs
@@ -23,5 +23,24 @@ namespace RepoDb.Extensions
                 }
             }
         }
+
+        /// <summary>
+        /// Converts an <see cref="Array"/> object into an enumerable of objects.
+        /// </summary>
+        /// <param name="array">The array to be converted.</param>
+        /// <returns>An enumerable of objects.</returns>
+        public static IEnumerable<T> AsEnumerable<T>(this Array array)
+        {
+            if (array != null)
+            {
+                foreach (var value in array)
+                {
+                    if (value is T)
+                    {
+                        yield return (T)value;
+                    }
+                }
+            }
+        }
     }
 }
