3e7079c2ea329e9c944c0c28d925dce249630631
Michael Pendon <michael.c.pendon@outlook.com>
2019-11-05 18:46:04 +0100

    Extensive IntegrationTests for Max and MaxAll.

---
+++ RepoDb.SqLite/NetFramework/RepoDb.SqLite.Tests/RepoDb.SqLite.IntegrationTests/Operations/MaxAllTest.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoDb.SqLite.IntegrationTests.Models;
+using RepoDb.SqLite.IntegrationTests.Setup;
+using System;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace RepoDb.SqLite.IntegrationTests.Operations
+{
+    [TestClass]
+    public class MaxAllTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            Database.Initialize();
+            Cleanup();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Database.Cleanup();
+        }
+
+        #region DataEntity
+
+        #region Sync
+
+        [TestMethod]
+        public void TestMaxAllWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.MaxAll<CompleteTable>(e => e.ColumnInt);
+
+                // Assert
+                Assert.AreEqual(tables.Max(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void TestMaxAllWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.MaxAll<CompleteTable>(e => e.ColumnInt,
+                    hints: "WhatEver");
+            }
+        }
+
+        #endregion
+
+        #region Async
+
+        [TestMethod]
+        public void TestMaxAllAsyncWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.MaxAllAsync<CompleteTable>(e => e.ColumnInt).Result;
+
+                // Assert
+                Assert.AreEqual(tables.Max(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(AggregateException))]
+        public void TestMaxAllAsyncWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.MaxAllAsync<CompleteTable>(e => e.ColumnInt,
+                    hints: "WhatEver").Wait();
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region TableName
+
+        #region Sync
+
+        [TestMethod]
+        public void TestMaxAllViaTableNameWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.MaxAll(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt));
+
+                // Assert
+                Assert.AreEqual(tables.Max(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void TestMaxAllViaTableNameWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.MaxAll(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    hints: "WhatEver");
+            }
+        }
+
+        #endregion
+
+        #region Async
+
+        [TestMethod]
+        public void TestMaxAllAsyncViaTableNameWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.MaxAllAsync(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt)).Result;
+
+                // Assert
+                Assert.AreEqual(tables.Max(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void TestMaxAllAsyncViaTableNameWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.MaxAllAsync(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    hints: "WhatEver").Wait();
+            }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
---
+++ RepoDb.SqLite/NetFramework/RepoDb.SqLite.Tests/RepoDb.SqLite.IntegrationTests/Operations/MaxTest.cs
@@ -0,0 +1,257 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoDb.Enumerations;
+using RepoDb.SqLite.IntegrationTests.Models;
+using RepoDb.SqLite.IntegrationTests.Setup;
+using System;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace RepoDb.SqLite.IntegrationTests.Operations
+{
+    [TestClass]
+    public class MaxTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            Database.Initialize();
+            Cleanup();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Database.Cleanup();
+        }
+
+        #region DataEntity
+
+        #region Sync
+
+        [TestMethod]
+        public void TestMaxWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.Max<CompleteTable>(e => e.ColumnInt,
+                    (object)null);
+
+                // Assert
+                Assert.AreEqual(tables.Max(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod]
+        public void TestMaxWithExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var ids = new[] { tables.First().Id, tables.Last().Id };
+                var result = connection.Max<CompleteTable>(e => e.ColumnInt,
+                    e => ids.Contains(e.Id));
+
+                // Assert
+                Assert.AreEqual(tables.Where(e => ids.Contains(e.Id)).Max(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void TestMaxWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.Max<CompleteTable>(e => e.ColumnInt,
+                    (object)null,
+                    hints: "WhatEver");
+            }
+        }
+
+        #endregion
+
+        #region Async
+
+        [TestMethod]
+        public void TestMaxAsyncWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.MaxAsync<CompleteTable>(e => e.ColumnInt,
+                    (object)null).Result;
+
+                // Assert
+                Assert.AreEqual(tables.Max(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod]
+        public void TestMaxAsyncWithExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var ids = new[] { tables.First().Id, tables.Last().Id };
+                var result = connection.MaxAsync<CompleteTable>(e => e.ColumnInt,
+                    e => ids.Contains(e.Id)).Result;
+
+                // Assert
+                Assert.AreEqual(tables.Where(e => ids.Contains(e.Id)).Max(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(AggregateException))]
+        public void TestMaxAsyncWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.MaxAsync<CompleteTable>(e => e.ColumnInt,
+                    (object)null,
+                    hints: "WhatEver").Wait();
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region TableName
+
+        #region Sync
+
+        [TestMethod]
+        public void TestMaxViaTableNameWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.Max(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    (object)null);
+
+                // Assert
+                Assert.AreEqual(tables.Max(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod]
+        public void TestMaxViaTableNameWithExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var ids = new[] { tables.First().Id, tables.Last().Id };
+                var result = connection.Max(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    new QueryField("Id", Operation.In, ids));
+
+                // Assert
+                Assert.AreEqual(tables.Where(e => ids.Contains(e.Id)).Max(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void TestMaxViaTableNameWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.Max(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    (object)null,
+                    hints: "WhatEver");
+            }
+        }
+
+        #endregion
+
+        #region Async
+
+        [TestMethod]
+        public void TestMaxAsyncViaTableNameWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.MaxAsync(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    (object)null).Result;
+
+                // Assert
+                Assert.AreEqual(tables.Max(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod]
+        public void TestMaxAsyncViaTableNameWithExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var ids = new[] { tables.First().Id, tables.Last().Id };
+                var result = connection.MaxAsync(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    new QueryField("Id", Operation.In, ids)).Result;
+
+                // Assert
+                Assert.AreEqual(tables.Where(e => ids.Contains(e.Id)).Max(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void TestMaxAsyncViaTableNameWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.MaxAsync(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    (object)null,
+                    hints: "WhatEver").Wait();
+            }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
