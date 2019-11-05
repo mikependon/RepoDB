48c1e0490bf5d4ed2c456f763b4c2097d50fa6bc
Michael Pendon <michael.c.pendon@outlook.com>
2019-11-05 18:44:58 +0100

    Extensive IntegrationTests for AverageAll.

---
+++ RepoDb.SqLite/NetFramework/RepoDb.SqLite.Tests/RepoDb.SqLite.IntegrationTests/Operations/AverageAllTest.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoDb.SqLite.IntegrationTests.Models;
+using RepoDb.SqLite.IntegrationTests.Setup;
+using System;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace RepoDb.SqLite.IntegrationTests.Operations
+{
+    [TestClass]
+    public class AverageAllTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            Database.Initialize();
+            Cleanup();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Database.Cleanup();
+        }
+
+        #region DataEntity
+
+        #region Sync
+
+        [TestMethod]
+        public void TestAverageAllWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.AverageAll<CompleteTable>(e => e.ColumnInt);
+
+                // Assert
+                Assert.AreEqual(tables.Average(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void TestAverageAllWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.AverageAll<CompleteTable>(e => e.ColumnInt,
+                    hints: "WhatEver");
+            }
+        }
+
+        #endregion
+
+        #region Async
+
+        [TestMethod]
+        public void TestAverageAllAsyncWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.AverageAllAsync<CompleteTable>(e => e.ColumnInt).Result;
+
+                // Assert
+                Assert.AreEqual(tables.Average(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(AggregateException))]
+        public void TestAverageAllAsyncWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.AverageAllAsync<CompleteTable>(e => e.ColumnInt,
+                    hints: "WhatEver").Wait();
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region TableName
+
+        #region Sync
+
+        [TestMethod]
+        public void TestAverageAllViaTableNameWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.AverageAll(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt));
+
+                // Assert
+                Assert.AreEqual(tables.Average(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void TestAverageAllViaTableNameWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.AverageAll(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    hints: "WhatEver");
+            }
+        }
+
+        #endregion
+
+        #region Async
+
+        [TestMethod]
+        public void TestAverageAllAsyncViaTableNameWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.AverageAllAsync(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt)).Result;
+
+                // Assert
+                Assert.AreEqual(tables.Average(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void TestAverageAllAsyncViaTableNameWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.AverageAllAsync(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    hints: "WhatEver").Wait();
+            }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
