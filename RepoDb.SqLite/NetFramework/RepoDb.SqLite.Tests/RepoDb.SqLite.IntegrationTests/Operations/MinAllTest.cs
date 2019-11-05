4888a2138479f83f5190fedbdadf5d41d1ec7123
Michael Pendon <michael.c.pendon@outlook.com>
2019-11-05 18:47:14 +0100

    Extensive IntegrationTests for Min and MinAll.

---
+++ RepoDb.SqLite/NetFramework/RepoDb.SqLite.Tests/RepoDb.SqLite.IntegrationTests/Operations/MinAllTest.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoDb.SqLite.IntegrationTests.Models;
+using RepoDb.SqLite.IntegrationTests.Setup;
+using System;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace RepoDb.SqLite.IntegrationTests.Operations
+{
+    [TestClass]
+    public class MinAllTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            Database.Initialize();
+            Cleanup();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Database.Cleanup();
+        }
+
+        #region DataEntity
+
+        #region Sync
+
+        [TestMethod]
+        public void TestMinAllWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.MinAll<CompleteTable>(e => e.ColumnInt);
+
+                // Assert
+                Assert.AreEqual(tables.Min(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void TestMinAllWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.MinAll<CompleteTable>(e => e.ColumnInt,
+                    hints: "WhatEver");
+            }
+        }
+
+        #endregion
+
+        #region Async
+
+        [TestMethod]
+        public void TestMinAllAsyncWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.MinAllAsync<CompleteTable>(e => e.ColumnInt).Result;
+
+                // Assert
+                Assert.AreEqual(tables.Min(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(AggregateException))]
+        public void TestMinAllAsyncWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.MinAllAsync<CompleteTable>(e => e.ColumnInt,
+                    hints: "WhatEver").Wait();
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region TableName
+
+        #region Sync
+
+        [TestMethod]
+        public void TestMinAllViaTableNameWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.MinAll(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt));
+
+                // Assert
+                Assert.AreEqual(tables.Min(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void TestMinAllViaTableNameWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.MinAll(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    hints: "WhatEver");
+            }
+        }
+
+        #endregion
+
+        #region Async
+
+        [TestMethod]
+        public void TestMinAllAsyncViaTableNameWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.MinAllAsync(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt)).Result;
+
+                // Assert
+                Assert.AreEqual(tables.Min(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void TestMinAllAsyncViaTableNameWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.MinAllAsync(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    hints: "WhatEver").Wait();
+            }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
---
+++ RepoDb.SqLite/NetFramework/RepoDb.SqLite.Tests/RepoDb.SqLite.IntegrationTests/Operations/MinTest.cs
@@ -0,0 +1,257 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoDb.Enumerations;
+using RepoDb.SqLite.IntegrationTests.Models;
+using RepoDb.SqLite.IntegrationTests.Setup;
+using System;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace RepoDb.SqLite.IntegrationTests.Operations
+{
+    [TestClass]
+    public class MinTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            Database.Initialize();
+            Cleanup();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Database.Cleanup();
+        }
+
+        #region DataEntity
+
+        #region Sync
+
+        [TestMethod]
+        public void TestMinWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.Min<CompleteTable>(e => e.ColumnInt,
+                    (object)null);
+
+                // Assert
+                Assert.AreEqual(tables.Min(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod]
+        public void TestMinWithExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var ids = new[] { tables.First().Id, tables.Last().Id };
+                var result = connection.Min<CompleteTable>(e => e.ColumnInt,
+                    e => ids.Contains(e.Id));
+
+                // Assert
+                Assert.AreEqual(tables.Where(e => ids.Contains(e.Id)).Min(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void TestMinWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.Min<CompleteTable>(e => e.ColumnInt,
+                    (object)null,
+                    hints: "WhatEver");
+            }
+        }
+
+        #endregion
+
+        #region Async
+
+        [TestMethod]
+        public void TestMinAsyncWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.MinAsync<CompleteTable>(e => e.ColumnInt,
+                    (object)null).Result;
+
+                // Assert
+                Assert.AreEqual(tables.Min(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod]
+        public void TestMinAsyncWithExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var ids = new[] { tables.First().Id, tables.Last().Id };
+                var result = connection.MinAsync<CompleteTable>(e => e.ColumnInt,
+                    e => ids.Contains(e.Id)).Result;
+
+                // Assert
+                Assert.AreEqual(tables.Where(e => ids.Contains(e.Id)).Min(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(AggregateException))]
+        public void TestMinAsyncWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.MinAsync<CompleteTable>(e => e.ColumnInt,
+                    (object)null,
+                    hints: "WhatEver").Wait();
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region TableName
+
+        #region Sync
+
+        [TestMethod]
+        public void TestMinViaTableNameWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.Min(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    (object)null);
+
+                // Assert
+                Assert.AreEqual(tables.Min(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod]
+        public void TestMinViaTableNameWithExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var ids = new[] { tables.First().Id, tables.Last().Id };
+                var result = connection.Min(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    new QueryField("Id", Operation.In, ids));
+
+                // Assert
+                Assert.AreEqual(tables.Where(e => ids.Contains(e.Id)).Min(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void TestMinViaTableNameWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.Min(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    (object)null,
+                    hints: "WhatEver");
+            }
+        }
+
+        #endregion
+
+        #region Async
+
+        [TestMethod]
+        public void TestMinAsyncViaTableNameWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.MinAsync(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    (object)null).Result;
+
+                // Assert
+                Assert.AreEqual(tables.Min(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod]
+        public void TestMinAsyncViaTableNameWithExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var ids = new[] { tables.First().Id, tables.Last().Id };
+                var result = connection.MinAsync(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    new QueryField("Id", Operation.In, ids)).Result;
+
+                // Assert
+                Assert.AreEqual(tables.Where(e => ids.Contains(e.Id)).Min(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void TestMinAsyncViaTableNameWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.MinAsync(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    (object)null,
+                    hints: "WhatEver").Wait();
+            }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
