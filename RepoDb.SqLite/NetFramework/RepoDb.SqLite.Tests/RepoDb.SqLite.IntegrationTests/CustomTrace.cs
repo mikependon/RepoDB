943e5a65f4ed36861081a3a8860f58e679e6e21f
Michael Pendon <michael.c.pendon@outlook.com>
2019-11-05 18:39:49 +0100

    Extensive IntegrationTests for Average.

---
+++ RepoDb.SqLite/NetFramework/RepoDb.SqLite.Tests/RepoDb.SqLite.IntegrationTests/CustomTrace.cs
@@ -0,0 +1,287 @@
+﻿using RepoDb.Interfaces;
+
+namespace RepoDb.SqLite.IntegrationTests
+{
+    public class CustomTrace : ITrace
+    {
+        public void AfterAverage(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterAverageAll(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterBatchQuery(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterBulkInsert(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterCount(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterCountAll(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterDelete(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterDeleteAll(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterExecuteNonQuery(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterExecuteQuery(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterExecuteReader(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterExecuteScalar(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterInsert(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterInsertAll(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterMax(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterMaxAll(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterMerge(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterMergeAll(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterMin(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterMinAll(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterQuery(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterQueryAll(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterQueryMultiple(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterSum(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterSumAll(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterTruncate(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterUpdate(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void AfterUpdateAll(TraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeAverage(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeAverageAll(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeBatchQuery(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeBulkInsert(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeCount(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeCountAll(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeDelete(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeDeleteAll(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeExecuteNonQuery(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeExecuteQuery(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeExecuteReader(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeExecuteScalar(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeInsert(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeInsertAll(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeMax(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeMaxAll(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeMerge(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeMergeAll(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeMin(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeMinAll(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeQuery(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeQueryAll(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeQueryMultiple(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeSum(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeSumAll(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeTruncate(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeUpdate(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+
+        public void BeforeUpdateAll(CancellableTraceLog log)
+        {
+            var statement = log.Statement;
+        }
+    }
+}
---
+++ RepoDb.SqLite/NetFramework/RepoDb.SqLite.Tests/RepoDb.SqLite.IntegrationTests/Operations/AverageTest.cs
@@ -0,0 +1,257 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoDb.Enumerations;
+using RepoDb.SqLite.IntegrationTests.Models;
+using RepoDb.SqLite.IntegrationTests.Setup;
+using System;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace RepoDb.SqLite.IntegrationTests.Operations
+{
+    [TestClass]
+    public class AverageTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            Database.Initialize();
+            Cleanup();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Database.Cleanup();
+        }
+
+        #region DataEntity
+
+        #region Sync
+
+        [TestMethod]
+        public void TestAverageWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.Average<CompleteTable>(e => e.ColumnInt,
+                    (object)null);
+
+                // Assert
+                Assert.AreEqual(tables.Average(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod]
+        public void TestAverageWithExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var ids = new[] { tables.First().Id, tables.Last().Id };
+                var result = connection.Average<CompleteTable>(e => e.ColumnInt,
+                    e => ids.Contains(e.Id));
+
+                // Assert
+                Assert.AreEqual(tables.Where(e => ids.Contains(e.Id)).Average(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void TestAverageWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.Average<CompleteTable>(e => e.ColumnInt,
+                    (object)null,
+                    hints: "WhatEver");
+            }
+        }
+
+        #endregion
+
+        #region Async
+
+        [TestMethod]
+        public void TestAverageAsyncWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.AverageAsync<CompleteTable>(e => e.ColumnInt,
+                    (object)null).Result;
+
+                // Assert
+                Assert.AreEqual(tables.Average(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod]
+        public void TestAverageAsyncWithExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var ids = new[] { tables.First().Id, tables.Last().Id };
+                var result = connection.AverageAsync<CompleteTable>(e => e.ColumnInt,
+                    e => ids.Contains(e.Id)).Result;
+
+                // Assert
+                Assert.AreEqual(tables.Where(e => ids.Contains(e.Id)).Average(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(AggregateException))]
+        public void TestAverageAsyncWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.AverageAsync<CompleteTable>(e => e.ColumnInt,
+                    (object)null,
+                    hints: "WhatEver").Wait();
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region TableName
+
+        #region Sync
+
+        [TestMethod]
+        public void TestAverageViaTableNameWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.Average(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    (object)null);
+
+                // Assert
+                Assert.AreEqual(tables.Average(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod]
+        public void TestAverageViaTableNameWithExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var ids = new[] { tables.First().Id, tables.Last().Id };
+                var result = connection.Average(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    new QueryField("Id", Operation.In, ids));
+
+                // Assert
+                Assert.AreEqual(tables.Where(e => ids.Contains(e.Id)).Average(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void TestAverageViaTableNameWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.Average(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    (object)null,
+                    hints: "WhatEver");
+            }
+        }
+
+        #endregion
+
+        #region Async
+
+        [TestMethod]
+        public void TestAverageAsyncViaTableNameWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.AverageAsync(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    (object)null).Result;
+
+                // Assert
+                Assert.AreEqual(tables.Average(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod]
+        public void TestAverageAsyncViaTableNameWithExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var ids = new[] { tables.First().Id, tables.Last().Id };
+                var result = connection.AverageAsync(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    new QueryField("Id", Operation.In, ids)).Result;
+
+                // Assert
+                Assert.AreEqual(tables.Where(e => ids.Contains(e.Id)).Average(e => e.ColumnInt), result);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(NotSupportedException))]
+        public void TestAverageAsyncViaTableNameWithHints()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                connection.AverageAsync(ClassMappedNameCache.Get<CompleteTable>(),
+                    Field.Parse<CompleteTable>(e => e.ColumnInt),
+                    (object)null,
+                    hints: "WhatEver").Wait();
+            }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
