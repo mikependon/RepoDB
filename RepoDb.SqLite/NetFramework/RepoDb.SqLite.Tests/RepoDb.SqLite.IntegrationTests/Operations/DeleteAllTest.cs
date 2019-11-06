e5c77da1f51e42ca3811c2c5a9b9c5c18210c329
Michael Pendon <michael.c.pendon@outlook.com>
2019-11-06 22:38:43 +0100

    Added extensive Integration Tests for Delete and DeleteAll.

---
+++ RepoDb.SqLite/NetFramework/RepoDb.SqLite.Tests/RepoDb.SqLite.IntegrationTests/Operations/DeleteAllTest.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoDb.SqLite.IntegrationTests.Models;
+using RepoDb.SqLite.IntegrationTests.Setup;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace RepoDb.SqLite.IntegrationTests.Operations
+{
+    [TestClass]
+    public class DeleteAllTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            Database.Initialize();
+            Cleanup();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Database.Cleanup();
+        }
+
+        #region DataEntity
+
+        #region Sync
+
+        [TestMethod]
+        public void TestDeleteAll()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.DeleteAll<CompleteTable>();
+
+                // Assert
+                Assert.AreEqual(tables.Count(), result);
+            }
+        }
+
+        #endregion
+
+        #region Async
+
+        [TestMethod]
+        public void TestDeleteAllAsyncWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.DeleteAllAsync<CompleteTable>().Result;
+
+                // Assert
+                Assert.AreEqual(tables.Count(), result);
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region TableName
+
+        #region Sync
+
+        [TestMethod]
+        public void TestDeleteAllViaTableNameWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.DeleteAll(ClassMappedNameCache.Get<CompleteTable>());
+
+                // Assert
+                Assert.AreEqual(tables.Count(), result);
+            }
+        }
+
+        #endregion
+
+        #region Async
+
+        [TestMethod]
+        public void TestDeleteAllAsyncViaTableNameWithoutExpression()
+        {
+            // Setup
+            var tables = Database.CreateCompleteTables(10);
+
+            using (var connection = new SQLiteConnection(Database.ConnectionString))
+            {
+                // Act
+                var result = connection.DeleteAllAsync(ClassMappedNameCache.Get<CompleteTable>()).Result;
+
+                // Assert
+                Assert.AreEqual(tables.Count(), result);
+            }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
