a858cf369f36504cba3722a98bf6c3e7c41d2d0a
Michael Camara Pendon <michael.c.pendon@outlook.com>
2020-01-08 07:14:13 +0100

    Added int as return type.

--- RepoDb.Core/RepoDb/Operations/BaseRepository/InsertAll.cs
+++ RepoDb.Core/RepoDb/Operations/BaseRepository/InsertAll.cs
@@ -19,11 +19,11 @@ namespace RepoDb
         /// <param name="transaction">The transaction to be used.</param>
         /// <param name="batchSize">The batch size of the insertion.</param>
         /// <returns>The number of inserted rows.</returns>
-        public void InsertAll(IEnumerable<TEntity> entities,
+        public int InsertAll(IEnumerable<TEntity> entities,
             int batchSize = Constant.DefaultBatchOperationSize,
             IDbTransaction transaction = null)
         {
-            DbRepository.InsertAll<TEntity>(entities: entities,
+            return DbRepository.InsertAll<TEntity>(entities: entities,
                 batchSize: batchSize,
                 transaction: transaction);
         }
@@ -39,7 +39,7 @@ namespace RepoDb
         /// <param name="batchSize">The batch size of the insertion.</param>
         /// <param name="transaction">The transaction to be used.</param>
         /// <returns>The number of inserted rows.</returns>
-        public Task InsertAllAsync(IEnumerable<TEntity> entities,
+        public Task<int> InsertAllAsync(IEnumerable<TEntity> entities,
             int batchSize = Constant.DefaultBatchOperationSize,
             IDbTransaction transaction = null)
         {
