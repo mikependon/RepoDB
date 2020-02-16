bf305eb95411f4a58c2e4b80879015323117737d
Michael Pendon <michael.c.pendon@outlook.com>
2020-02-16 14:36:30 +0100

    - Feature: Introduce the SQL Server support to SqlParameter.SqlDbType via MicrosoftSqlServerTypeMapAttribute class (for Microsoft.Data.SqlClient) and SystemSqlServerTypeMapAttribute class (for System.Data.SqlClient).

---
+++ RepoDb.SqlServer/RepoDb.SqlServer/Attributes/MicrosoftSqlServerTypeMapAttribute.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+
+namespace RepoDb.Attributes
+{
+    /// <summary>
+    /// An attribute used to define a mapping of .NET CLR <see cref="Type"/> into its equivalent <see cref="SqlDbType"/> value.
+    /// </summary>
+    public class MicrosoftSqlServerTypeMapAttribute : Attribute
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="MicrosoftSqlServerTypeMapAttribute"/> class.
+        /// </summary>
+        /// <param name="dbType">A target <see cref="SqlDbType"/> value.</param>
+        public MicrosoftSqlServerTypeMapAttribute(SqlDbType dbType)
+        {
+            DbType = dbType;
+            ParameterType = typeof(SqlParameter);
+        }
+
+        /// <summary>
+        /// Gets a <see cref="SqlDbType"/> that is currently mapped.
+        /// </summary>
+        public SqlDbType DbType { get; }
+
+        /// <summary>
+        /// Gets the represented <see cref="Type"/> of the <see cref="SqlParameter"/>.
+        /// </summary>
+        public Type ParameterType { get; }
+    }
+}
---
+++ RepoDb.SqlServer/RepoDb.SqlServer/Attributes/SystemSqlServerTypeMapAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace RepoDb.Attributes
+{
+    /// <summary>
+    /// An attribute used to define a mapping of .NET CLR <see cref="Type"/> into its equivalent <see cref="SqlDbType"/> value.
+    /// </summary>
+    public class SystemSqlServerTypeMapAttribute : Attribute
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="SystemSqlServerTypeMapAttribute"/> class.
+        /// </summary>
+        /// <param name="dbType">A target <see cref="SqlDbType"/> value.</param>
+        public SystemSqlServerTypeMapAttribute(SqlDbType dbType)
+        {
+            DbType = dbType;
+            ParameterType = typeof(SqlParameter);
+        }
+
+        /// <summary>
+        /// Gets a <see cref="SqlDbType"/> that is currently mapped.
+        /// </summary>
+        public SqlDbType DbType { get; }
+
+        /// <summary>
+        /// Gets the represented <see cref="Type"/> of the <see cref="SqlParameter"/>.
+        /// </summary>
+        public Type ParameterType { get; }
+    }
+}
