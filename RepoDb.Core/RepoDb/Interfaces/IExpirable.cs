6beda9bb4c0c6f840590e868063042ce18bce0d2
Michael Pendon <michael.c.pendon@outlook.com>
2020-03-27 16:58:45 +0100

    Added a new instance IExpirable.

---
+++ RepoDb.Core/RepoDb/Interfaces/IExpirable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RepoDb.Interfaces
+{
+    /// <summary>
+    /// An interface used to mark the class to be expirable.
+    /// </summary>
+    public interface IExpirable
+    {
+        /// <summary>
+        /// Gets the created timestamp of this class.
+        /// </summary>
+        DateTime CreatedDate { get; }
+
+        /// <summary>
+        /// Gets or sets the expiration date of this class.
+        /// </summary>
+        DateTime Expiration { get; set; }
+
+        /// <summary>
+        /// Gets a value whether this class is expired.
+        /// </summary>
+        /// <returns>A boolean value that indicate whether this class is expired.</returns>
+        bool IsExpired();
+    }
+}
