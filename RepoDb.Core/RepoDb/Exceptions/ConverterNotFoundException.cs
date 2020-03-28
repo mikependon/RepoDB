9b3a5e3e6f9a6b086da7c38a4801432fcee5423c
Michael Pendon <michael.c.pendon@outlook.com>
2020-03-28 14:25:11 +0100

    Added the class named ConverterNotFoundException.

---
+++ RepoDb.Core/RepoDb/Exceptions/ConverterNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RepoDb.Exceptions
+{
+    /// <summary>
+    /// An exception that is being thrown when the converter is not found.
+    /// </summary>
+    public class ConverterNotFoundException : Exception
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="ConverterNotFoundException"/> class.
+        /// </summary>
+        /// <param name="message">The exception message.</param>
+        public ConverterNotFoundException(string message)
+            : base(message) { }
+    }
+}
