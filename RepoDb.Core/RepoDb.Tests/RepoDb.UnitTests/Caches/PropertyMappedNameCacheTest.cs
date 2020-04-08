3b0df8b4270dc33aae99390bd50e721b26754f0c
Michael Pendon <michael.c.pendon@outlook.com>
2020-04-08 18:00:57 +0200

    Added the PropertyMappedNameCacheTest test class.

---
+++ RepoDb.Core/RepoDb.Tests/RepoDb.UnitTests/Caches/PropertyMappedNameCacheTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoDb.Attributes;
+using System.Linq;
+
+namespace RepoDb.UnitTests.Others
+{
+    [TestClass]
+    public partial class PropertyMappedNameCacheTest
+    {
+        private class PropertyMappedNameCacheTestClass
+        {
+            public string ColumnString { get; set; }
+            [Map("PropertyName")]
+            public string PropertyString { get; set; }
+        }
+
+        [TestMethod]
+        public void TestWithoutMapAttribute()
+        {
+            // Act
+            var property = PropertyCache.Get<PropertyMappedNameCacheTestClass>()
+                .First(p => p.PropertyInfo.Name == "ColumnString");
+            var expected = "ColumnString";
+
+            // Assert
+            Assert.AreEqual(expected, property.GetMappedName());
+        }
+
+        [TestMethod]
+        public void TestWithMapAttribute()
+        {
+            // Act
+            var property = PropertyCache.Get<PropertyMappedNameCacheTestClass>()
+                .First(p => p.PropertyInfo.Name == "PropertyString");
+            var expected = "PropertyName";
+
+            // Assert
+            Assert.AreEqual(expected, property.GetMappedName());
+        }
+    }
+}
