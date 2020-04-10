0ceb4d6ff93e7526b1078f13d55e0b8068a6274a
Michael Pendon <michael.c.pendon@outlook.com>
2020-04-10 13:25:59 +0200

    Added a new exception for the properties that are not found.

---
+++ RepoDb.Core/RepoDb/Exceptions/PropertyNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RepoDb.Exceptions
+{
+    /// <summary>
+    /// An exception that is being thrown if the target property is not found.
+    /// </summary>
+    public class PropertyNotFoundException : Exception
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="PropertyNotFoundException"/> class.
+        /// </summary>
+        /// <param name="message">The exception message.</param>
+        public PropertyNotFoundException(string message)
+            : base(message) { }
+    }
+}
