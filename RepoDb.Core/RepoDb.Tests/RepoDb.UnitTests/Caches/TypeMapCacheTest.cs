004a817bd6799bb5e1b6b28ce531273bd59408ec
Michael Pendon <michael.c.pendon@outlook.com>
2020-04-11 14:23:24 +0200

    Enhancement: Add the TypeMapCache class. #418 - This is support as well the #415 and #399

---
+++ RepoDb.Core/RepoDb.Tests/RepoDb.UnitTests/Caches/TypeMapCacheTest.cs
@@ -0,0 +1,234 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoDb.Attributes;
+using System.Data;
+using System.Linq;
+
+namespace RepoDb.UnitTests.Others
+{
+    [TestClass]
+    public partial class TypeMapCacheTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            Cleanup();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            TypeMapper.Flush();
+            TypeMapCache.Flush();
+        }
+
+        #region SubClasses
+
+        private class TypeMapCacheTestClass
+        {
+            public string ColumnString { get; set; }
+            [TypeMap(DbType.String)]
+            public string PropertyString { get; set; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        #region Type Level
+
+        [TestMethod]
+        public void TestWithoutMapping()
+        {
+            // Act
+            var result = TypeMapCache.Get<TypeMapCacheTest>();
+            var expected = (DbType?)null;
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestWithMapping()
+        {
+            // Setup
+            TypeMapper.Add<TypeMapCacheTestClass>(DbType.Object);
+
+            // Act
+            var result = TypeMapCache.Get<TypeMapCacheTestClass>();
+            var expected = DbType.Object;
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        #endregion
+
+        #region Property Level
+
+        /*
+         * No MapAttribute
+         */
+
+        [TestMethod]
+        public void TestTypeMapperPropertyMappingViaPropertyName()
+        {
+            // Setup
+            var propertyName = "ColumnString";
+            TypeMapper.Add<TypeMapCacheTestClass>(propertyName, DbType.StringFixedLength);
+
+            // Act
+            var actual = TypeMapCache.Get<TypeMapCacheTestClass>(propertyName);
+            var expected = DbType.StringFixedLength;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestTypeMapperPropertyMappingViaField()
+        {
+            // Setup
+            var field = new Field("ColumnString");
+            TypeMapper.Add<TypeMapCacheTestClass>(field, DbType.StringFixedLength);
+
+            // Act
+            var actual = TypeMapCache.Get<TypeMapCacheTestClass>(field);
+            var expected = DbType.StringFixedLength;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestTypeMapperPropertyMappingViaExpression()
+        {
+            // Setup
+            TypeMapper.Add<TypeMapCacheTestClass>(e => e.ColumnString, DbType.StringFixedLength);
+
+            // Act
+            var actual = TypeMapCache.Get<TypeMapCacheTestClass>(e => e.ColumnString);
+            var expected = DbType.StringFixedLength;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestTypeMapperPropertyMappingViaClassProperty()
+        {
+            // Setup
+            var classProperty = PropertyCache.Get<TypeMapCacheTestClass>()
+                .First(p => p.PropertyInfo.Name == "ColumnString");
+            TypeMapper.Add(classProperty, DbType.StringFixedLength);
+
+            // Act
+            var actual = TypeMapCache.Get(classProperty);
+            var expected = DbType.StringFixedLength;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestTypeMapperPropertyMappingViaPropertyInfo()
+        {
+            // Setup
+            var propertyInfo = typeof(TypeMapCacheTestClass)
+                .GetProperties()
+                .First(p => p.Name == "ColumnString");
+            TypeMapper.Add(propertyInfo, DbType.StringFixedLength);
+
+            // Act
+            var actual = TypeMapCache.Get(propertyInfo);
+            var expected = DbType.StringFixedLength;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /*
+         * With MapAttribute
+         */
+
+        [TestMethod]
+        public void TestTypeMapperPropertyMappingViaPropertyNameWithMapAttribute()
+        {
+            // Setup
+            var propertyName = "PropertyString";
+            TypeMapper.Add<TypeMapCacheTestClass>(propertyName, DbType.StringFixedLength);
+
+            // Act
+            var actual = TypeMapCache.Get<TypeMapCacheTestClass>(propertyName);
+            var expected = DbType.String;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestTypeMapperPropertyMappingViaFieldWithMapAttribute()
+        {
+            // Setup
+            var field = new Field("PropertyString");
+            TypeMapper.Add<TypeMapCacheTestClass>(field, DbType.StringFixedLength);
+
+            // Act
+            var actual = TypeMapCache.Get<TypeMapCacheTestClass>(field);
+            var expected = DbType.String;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestTypeMapperPropertyMappingViaExpressionWithMapAttribute()
+        {
+            // Setup
+            TypeMapper.Add<TypeMapCacheTestClass>(e => e.PropertyString, DbType.StringFixedLength);
+
+            // Act
+            var actual = TypeMapCache.Get<TypeMapCacheTestClass>(e => e.PropertyString);
+            var expected = DbType.String;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestTypeMapperPropertyMappingViaClassPropertyWithMapAttribute()
+        {
+            // Setup
+            var classProperty = PropertyCache.Get<TypeMapCacheTestClass>()
+                .First(p => p.PropertyInfo.Name == "PropertyString");
+            TypeMapper.Add(classProperty, DbType.StringFixedLength);
+
+            // Act
+            var actual = TypeMapCache.Get(classProperty);
+            var expected = DbType.String;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestTypeMapperPropertyMappingViaPropertyInfoWithMapAttribute()
+        {
+            // Setup
+            var propertyInfo = typeof(TypeMapCacheTestClass)
+                .GetProperties()
+                .First(p => p.Name == "PropertyString");
+            TypeMapper.Add(propertyInfo, DbType.StringFixedLength);
+
+            // Act
+            var actual = TypeMapCache.Get(propertyInfo);
+            var expected = DbType.String;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
---
+++ RepoDb.Core/RepoDb/TypeMapCache.cs
@@ -0,0 +1,172 @@
+﻿using RepoDb.Attributes;
+using RepoDb.Extensions;
+using System;
+using System.Collections.Concurrent;
+using System.Data;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace RepoDb
+{
+    /// <summary>
+    /// A class that is being used to cache the mappings between the <see cref="DbType"/> object and .NET CLR type or class property.
+    /// </summary>
+    public static class TypeMapCache
+    {
+        #region Privates
+
+        private static readonly ConcurrentDictionary<int, DbType?> m_cache = new ConcurrentDictionary<int, DbType?>();
+
+        #endregion
+
+        #region Methods
+
+        #region TypeLevel
+
+        /// <summary>
+        /// Gets the cached <see cref="DbType"/> object that is being mapped on a specific .NET CLR type.
+        /// </summary>
+        /// <typeparam name="T">The target .NET CLR type.</typeparam>
+        /// <returns>The mapped <see cref="DbType"/> object of the .NET CLR type.</returns>
+        public static DbType? Get<T>() =>
+            Get(typeof(T));
+
+        /// <summary>
+        /// Gets the cached <see cref="DbType"/> object that is being mapped on a specific .NET CLR type.
+        /// </summary>
+        /// <param name="type">The target .NET CLR type.</param>
+        /// <returns>The mapped <see cref="DbType"/> object of the .NET CLR type.</returns>
+        public static DbType? Get(Type type)
+        {
+            // Validate
+            ThrowNullReferenceException(type, "Type");
+
+            // Variables
+            var key = type.GetUnderlyingType().FullName.GetHashCode();
+            var result = (DbType?)null;
+
+            // Try get the value
+            if (m_cache.TryGetValue(key, out result) == false)
+            {
+                result = TypeMapper.Get(type);
+                m_cache.TryAdd(key, result);
+            }
+
+            // Return the value
+            return result;
+        }
+
+        #endregion
+
+        #region Property Level
+
+        /// <summary>
+        /// Gets the cached <see cref="DbType"/> object that is being mapped on a specific class property (via expression).
+        /// </summary>
+        /// <typeparam name="T">The type of the entity.</typeparam>
+        /// <param name="expression">The expression to be parsed.</param>
+        /// <returns>The mapped <see cref="DbType"/> object of the property.</returns>
+        public static DbType? Get<T>(Expression<Func<T, object>> expression)
+            where T : class =>
+            Get(ExpressionExtension.GetProperty<T>(expression));
+
+        /// <summary>
+        /// Gets the cached <see cref="DbType"/> object that is being mapped on a specific class property (via property name).
+        /// </summary>
+        /// <typeparam name="T">The type of the entity.</typeparam>
+        /// <param name="propertyName">The name of the property.</param>
+        /// <returns>The mapped <see cref="DbType"/> object of the property.</returns>
+        public static DbType? Get<T>(string propertyName)
+            where T : class =>
+            Get(TypeExtension.GetProperty<T>(propertyName));
+
+        /// <summary>
+        /// Gets the cached <see cref="DbType"/> object that is being mapped on a specific class property (via <see cref="Field"/> object).
+        /// </summary>
+        /// <typeparam name="T">The type of the entity.</typeparam>
+        /// <param name="field">The instance of <see cref="Field"/> object.</param>
+        /// <returns>The mapped <see cref="DbType"/> object of the property.</returns>
+        public static DbType? Get<T>(Field field)
+            where T : class =>
+            Get(TypeExtension.GetProperty<T>(field.Name));
+
+        /// <summary>
+        /// Gets the cached <see cref="DbType"/> object that is being mapped on a specific <see cref="ClassProperty"/> object.
+        /// </summary>
+        /// <param name="classProperty">The instance of <see cref="ClassProperty"/>.</param>
+        /// <returns>The mapped <see cref="DbType"/> object of the property.</returns>
+        public static DbType? Get(ClassProperty classProperty) =>
+            Get(classProperty.PropertyInfo);
+
+        /// <summary>
+        /// Gets the cached <see cref="DbType"/> object that is being mapped on a specific <see cref="PropertyInfo"/> object.
+        /// </summary>
+        /// <param name="propertyInfo">The instance of <see cref="PropertyInfo"/>.</param>
+        /// <returns>The mapped <see cref="DbType"/> object of the property.</returns>
+        public static DbType? Get(PropertyInfo propertyInfo)
+        {
+            // Validate
+            ThrowNullReferenceException(propertyInfo, "PropertyInfo");
+
+            // Variables
+            var key = propertyInfo.GenerateCustomizedHashCode();
+            var result = (DbType?)null;
+
+            // Try get the value
+            if (m_cache.TryGetValue(key, out result) == false)
+            {
+                // Get the attribute first
+                result = propertyInfo.GetCustomAttribute<TypeMapAttribute>()?.DbType;
+
+                // Get from the mapper (property level)
+                if (result == null)
+                {
+                    result = TypeMapper.Get(propertyInfo);
+                }
+
+                // Get from the mapper (type level)
+                if (result == null)
+                {
+                    result = TypeMapper.Get(propertyInfo.PropertyType);
+                }
+
+                // Add to cache
+                m_cache.TryAdd(key, result);
+            }
+
+            // Return the value
+            return result;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Helpers
+
+        /// <summary>
+        /// Flushes all the existing cached property mapped names.
+        /// </summary>
+        public static void Flush()
+        {
+            m_cache.Clear();
+        }
+
+        /// <summary>
+        /// Validates the target object presence.
+        /// </summary>
+        /// <typeparam name="T">The type of the object.</typeparam>
+        /// <param name="obj">The object to be checked.</param>
+        /// <param name="argument">The name of the argument.</param>
+        private static void ThrowNullReferenceException<T>(T obj,
+            string argument)
+        {
+            if (obj == null)
+            {
+                throw new NullReferenceException($"The argument '{argument}' cannot be null.");
+            }
+        }
+
+        #endregion
+    }
+}
