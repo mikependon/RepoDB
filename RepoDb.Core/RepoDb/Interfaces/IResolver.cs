575920a89d728590c4d6e4e95048564b188f3f10
Michael Pendon <michael.c.pendon@outlook.com>
2020-05-17 13:59:02 +0200

    Added an IResolver interface that does not require an input.

--- RepoDb.Core/RepoDb/Interfaces/IResolver.cs
+++ RepoDb.Core/RepoDb/Interfaces/IResolver.cs
@@ -3,6 +3,19 @@
     /// <summary>
     /// An interfaced used to mark a class to become a conversion resolver.
     /// </summary>
+    /// <typeparamref name="TResult">The type of the result value.</typeparamref>
+    public interface IResolver<TResult>
+    {
+        /// <summary>
+        /// Resolves an input value to a target result type.
+        /// </summary>
+        /// <returns>The resolved value.</returns>
+        TResult Resolve();
+    }
+
+    /// <summary>
+    /// An interfaced used to mark a class to become a conversion resolver.
+    /// </summary>
     /// <typeparamref name="TInput">The type of the input value.</typeparamref>
     /// <typeparamref name="TResult">The type of the result value.</typeparamref>
     public interface IResolver<TInput, TResult>
