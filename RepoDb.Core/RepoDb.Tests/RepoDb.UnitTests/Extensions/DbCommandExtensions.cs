0d0cc68dc628b9d34791d08737cb44952639d816
David Rothmann <david@rothmann.it>
2020-05-18 20:34:38 +0200

    Add triggering of property handler in dbparameters

---
+++ RepoDb.Core/RepoDb.Tests/RepoDb.UnitTests/Extensions/DbCommandExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoDb.Attributes;
+using RepoDb.Extensions;
+using RepoDb.Interfaces;
+
+namespace RepoDb.UnitTests.Extensions
+{
+    [TestClass]
+    public class DbCommandExtensionsTest
+    {
+        class TestClass
+        {
+            public Guid Id { get; set; } = Guid.NewGuid();
+        }
+
+        class StringToGuidPropertyHandler : IPropertyHandler<string, Guid>
+        {
+            public Guid Get(string input, ClassProperty property)
+            {
+                Guid.TryParse(input, out Guid output);
+                return output;
+            }
+
+            public string Set(Guid input, ClassProperty property)
+            {
+                return input.ToString();
+            }
+        }
+
+        [TestMethod]
+        public void CreateParametersPropertyHandlerWillBeUsedOnParameterResolution()
+        {
+            // Arrange
+            FluentMapper.Entity<TestClass>().PropertyHandler<StringToGuidPropertyHandler>(e => e.Id).;
+            var cmd = new SqlConnection().CreateCommand();
+
+            // Act
+            cmd.CreateParameters(new TestClass { Id = Guid.Parse("9963c864-ab4f-43f8-9dc9-43038565b971") });
+
+            // Assert
+            Assert.AreEqual(1, cmd.Parameters.Count);
+            Assert.AreEqual("9963c864-ab4f-43f8-9dc9-43038565b971", cmd.Parameters[0].Value);
+        }
+
+        [TestMethod]
+        public void CreateParametersQueryFieldPropertyHandlerWillBeUsedOnParameterResolution()
+        {
+            // Arrange
+            PropertyHandlerMapper.Add(typeof(Guid), new StringToGuidPropertyHandler(), true);
+            var cmd = new SqlConnection().CreateCommand();
+
+            // Act
+            cmd.CreateParameters(new QueryField("Id", Guid.Parse("9963c864-ab4f-43f8-9dc9-43038565b971")));
+
+            // Assert
+            Assert.AreEqual(1, cmd.Parameters.Count);
+            Assert.AreEqual("9963c864-ab4f-43f8-9dc9-43038565b971", cmd.Parameters[0].Value);
+        }
+    }
+}
