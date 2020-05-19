d7f52865e2507f8299c4fe03100efc5d615bc2fd
Michael Pendon <michael.c.pendon@outlook.com>
2020-05-19 06:52:19 +0200

    Create ClassMappedNameResolverTest.cs

---
+++ RepoDb.Core/RepoDb.Tests/RepoDb.UnitTests/Resolvers/ClassMappedNameResolverTest.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoDb.Attributes;
+using RepoDb.Resolvers;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RepoDb.UnitTests.Resolvers
+{
+    [TestClass]
+    public class ClassMappedNameResolverTest
+    {
+        #region SubClasses
+
+        private class EntityModel { }
+
+        [Map("[dbo].[Map]")]
+        private class EntityModelWithMapAttribute { }
+
+        [Table("[dbo].[Table]")]
+        private class EntityModelWithTableAttribute { }
+
+        [Map("[dbo].[Map]"), Table("[dbo].[Table]")]
+        private class EntityModelWithMapAndTableAttribute { }
+
+        #endregion
+
+        /*
+         * No Attributes
+         */
+
+        [TestMethod]
+        public void TestClassMappedNameResolverWithoutAttribute()
+        {
+            // Setup
+            var resolver = new ClassMappedNameResolver();
+
+            // Act
+            var result = resolver.Resolve(typeof(EntityModel));
+            var expected = "EntityModel";
+
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        /*
+         * With Attributes
+         */
+
+        [TestMethod]
+        public void TestClassMappedNameResolverWithMapAttribute()
+        {
+            // Setup
+            var resolver = new ClassMappedNameResolver();
+
+            // Act
+            var result = resolver.Resolve(typeof(EntityModelWithMapAttribute));
+            var expected = "[dbo].[Map]";
+
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestClassMappedNameResolverWithTableAttribute()
+        {
+            // Setup
+            var resolver = new ClassMappedNameResolver();
+
+            // Act
+            var result = resolver.Resolve(typeof(EntityModelWithTableAttribute));
+            var expected = "[dbo].[Table]";
+
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestClassMappedNameResolverWithMapAndTableAttribute()
+        {
+            // Setup
+            var resolver = new ClassMappedNameResolver();
+
+            // Act
+            var result = resolver.Resolve(typeof(EntityModelWithMapAndTableAttribute));
+            var expected = "[dbo].[Map]";
+
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        /*
+         * With Mappings
+         */
+
+        [TestMethod]
+        public void TestClassMappedNameResolverWithMapAttributeAndMappings()
+        {
+            // Setup
+            var resolver = new ClassMappedNameResolver();
+            FluentMapper
+                .Entity<EntityModelWithMapAttribute>()
+                .Table("[dbo].[Mapping]");
+
+            // Act
+            var result = resolver.Resolve(typeof(EntityModelWithMapAttribute));
+            var expected = "[dbo].[Map]";
+
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestClassMappedNameResolverWithTableAttributeAndMappings()
+        {
+            // Setup
+            var resolver = new ClassMappedNameResolver();
+            FluentMapper
+                .Entity<EntityModelWithTableAttribute>()
+                .Table("[dbo].[Mapping]");
+
+            // Act
+            var result = resolver.Resolve(typeof(EntityModelWithTableAttribute));
+            var expected = "[dbo].[Table]";
+
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
+    }
+}
