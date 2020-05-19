4309dba9553b2296eaec066b61e2ae601cc5b97e
Michael Pendon <michael.c.pendon@outlook.com>
2020-05-19 06:52:13 +0200

    Added more Unit Tests for Resolvers

---
+++ RepoDb.Core/RepoDb.Tests/RepoDb.UnitTests/Resolvers/PropertyHandlerPropertyLevelResolverTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace RepoDb.UnitTests.Resolvers
+{
+    [TestClass]
+    public class PropertyHandlerPropertyLevelResolverTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RepoDb.Core/RepoDb.Tests/RepoDb.UnitTests/Resolvers/TypeMapPropertyLevelResolverTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace RepoDb.UnitTests.Resolvers
+{
+    [TestClass]
+    public class TypeMapPropertyLevelResolverTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
