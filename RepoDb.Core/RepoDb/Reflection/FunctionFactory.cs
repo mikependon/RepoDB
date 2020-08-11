23255de0d35ab3195193b7bad68ea6a98614f5b0
Michael Camara Pendon <michael.c.pendon@outlook.com>
2020-08-11 08:58:53 +0200

    Removed the unnecessary methods for DB Provider specifics.

--- RepoDb.Core/RepoDb/Reflection/FunctionFactory.cs
+++ RepoDb.Core/RepoDb/Reflection/FunctionFactory.cs
@@ -14,8 +14,6 @@ namespace RepoDb.Reflection
     /// </summary>
     internal static class FunctionFactory
     {
-        #region CompileDataReaderToDataEntity
-
         /// <summary>
         /// Gets a compiled function that is used to convert the <see cref="DbDataReader"/> object into a list of data entity objects.
         /// </summary>
@@ -38,10 +36,6 @@ namespace RepoDb.Reflection
                 transaction,
                 enableValidation);
 
-        #endregion
-
-        #region CompileDataReaderToExpandoObject
-
         /// <summary>
         /// Gets a compiled function that is used to convert the <see cref="DbDataReader"/> object into a list of dynamic objects.
         /// </summary>
@@ -59,10 +53,6 @@ namespace RepoDb.Reflection
                 connection,
                 transaction);
 
-        #endregion
-
-        #region CompileDataEntityDbParameterSetter
-
         /// <summary>
         /// Gets a compiled function that is used to set the <see cref="DbParameter"/> objects of the <see cref="DbCommand"/> object based from the values of the data entity/dynamic object.
         /// </summary>
@@ -76,10 +66,6 @@ namespace RepoDb.Reflection
             IDbSetting dbSetting)
             where TEntity : class =>
             Compiler.CompileDataEntityDbParameterSetter<TEntity>(inputFields, outputFields, dbSetting);
-
-        #endregion
-
-        #region CompileDataEntityListDbParameterSetter
 
         /// <summary>
         /// Gets a compiled function that is used to set the <see cref="DbParameter"/> objects of the <see cref="DbCommand"/> object based from the values of the data entity/dynamic objects.
@@ -97,10 +83,6 @@ namespace RepoDb.Reflection
             where TEntity : class =>
             Compiler.CompileDataEntityListDbParameterSetter<TEntity>(inputFields, outputFields, batchSize, dbSetting);
 
-        #endregion
-
-        #region CompileDbCommandToProperty
-
         /// <summary>
         /// Gets a compiled function that is used to set the data entity object property value based from the value of <see cref="DbCommand"/> parameter object.
         /// </summary>
@@ -117,10 +99,6 @@ namespace RepoDb.Reflection
             where TEntity : class =>
             Compiler.CompileDbCommandToProperty<TEntity>(field, parameterName, index, dbSetting);
 
-        #endregion
-
-        #region CompileDataEntityPropertySetter
-
         /// <summary>
         /// Gets a compiled function that is used to set the data entity object property value.
         /// </summary>
@@ -130,283 +108,5 @@ namespace RepoDb.Reflection
         public static Action<TEntity, object> CompileDataEntityPropertySetter<TEntity>(Field field)
             where TEntity : class =>
             Compiler.CompileDataEntityPropertySetter<TEntity>(field);
-
-        #endregion
-
-        #region Other Data Providers Helpers
-
-        #region SqlServer (System)
-
-        /// <summary>
-        /// Gets the SystemSqlServerTypeMapAttribute if present.
-        /// </summary>
-        /// <param name="property">The instance of propery to inspect.</param>
-        /// <returns>The instance of SystemSqlServerTypeMapAttribute.</returns>
-        internal static Attribute GetSystemSqlServerTypeMapAttribute(ClassProperty property)
-        {
-            return property?
-                .PropertyInfo
-                .GetCustomAttributes()?
-                .FirstOrDefault(e =>
-                    e.GetType().FullName.Equals("RepoDb.Attributes.SystemSqlServerTypeMapAttribute"));
-        }
-
-        /// <summary>
-        /// Gets the value represented by the SystemSqlServerTypeMapAttribute.DbType property.
-        /// </summary>
-        /// <param name="attribute">The instance of SystemSqlServerTypeMapAttribute to extract.</param>
-        /// <returns>The value represented by the SystemSqlServerTypeMapAttribute.DbType property.</returns>
-        internal static object GetSystemSqlServerDbTypeFromAttribute(Attribute attribute)
-        {
-            if (attribute == null)
-            {
-                return null;
-            }
-            var type = attribute.GetType();
-            return type
-                .GetProperty("DbType")?
-                .GetValue(attribute);
-        }
-
-        /// <summary>
-        /// Gets the system type of System.Data.SqlClient.SqlParameter represented by SystemSqlServerTypeMapAttribute.ParameterType property.
-        /// </summary>
-        /// <param name="attribute">The instance of SystemSqlServerTypeMapAttribute to extract.</param>
-        /// <returns>The type of System.Data.SqlClient.SqlParameter represented by SystemSqlServerTypeMapAttribute.ParameterType property.</returns>
-        internal static Type GetSystemSqlServerParameterTypeFromAttribute(Attribute attribute)
-        {
-            if (attribute == null)
-            {
-                return null;
-            }
-            return (Type)attribute
-                .GetType()
-                .GetProperty("ParameterType")?
-                .GetValue(attribute);
-        }
-
-        /// <summary>
-        /// Gets the instance of <see cref="MethodInfo"/> represented by the SystemSqlServerTypeMapAttribute.DbType property.
-        /// </summary>
-        /// <param name="attribute">The instance of SystemSqlServerTypeMapAttribute to extract.</param>
-        /// <returns>The instance of <see cref="MethodInfo"/> represented by the SystemSqlServerTypeMapAttribute.DbType property.</returns>
-        internal static MethodInfo GetSystemSqlServerDbTypeFromAttributeSetMethod(Attribute attribute)
-        {
-            if (attribute == null)
-            {
-                return null;
-            }
-            return GetSystemSqlServerParameterTypeFromAttribute(attribute)?
-                .GetProperty("SqlDbType")?
-                .SetMethod;
-        }
-
-        #endregion
-
-        #region SqlServer (Microsoft)
-
-        /// <summary>
-        /// Gets the MicrosoftSqlServerTypeMapAttribute if present.
-        /// </summary>
-        /// <param name="property">The instance of propery to inspect.</param>
-        /// <returns>The instance of MicrosoftSqlServerTypeMapAttribute.</returns>
-        internal static Attribute GetMicrosoftSqlServerTypeMapAttribute(ClassProperty property)
-        {
-            return property?
-                .PropertyInfo
-                .GetCustomAttributes()?
-                .FirstOrDefault(e =>
-                    e.GetType().FullName.Equals("RepoDb.Attributes.MicrosoftSqlServerTypeMapAttribute"));
-        }
-
-        /// <summary>
-        /// Gets the value represented by the MicrosoftSqlServerTypeMapAttribute.DbType property.
-        /// </summary>
-        /// <param name="attribute">The instance of MicrosoftSqlServerTypeMapAttribute to extract.</param>
-        /// <returns>The value represented by the MicrosoftSqlServerTypeMapAttribute.DbType property.</returns>
-        internal static object GetMicrosoftSqlServerDbTypeFromAttribute(Attribute attribute)
-        {
-            if (attribute == null)
-            {
-                return null;
-            }
-            var type = attribute.GetType();
-            return type
-                .GetProperty("DbType")?
-                .GetValue(attribute);
-        }
-
-        /// <summary>
-        /// Gets the system type of Microsoft.Data.SqlClient.SqlParameter represented by MicrosoftSqlServerTypeMapAttribute.ParameterType property.
-        /// </summary>
-        /// <param name="attribute">The instance of MicrosoftSqlServerTypeMapAttribute to extract.</param>
-        /// <returns>The type of Microsoft.Data.SqlClient.SqlParameter represented by MicrosoftSqlServerTypeMapAttribute.ParameterType property.</returns>
-        internal static Type GetMicrosoftSqlServerParameterTypeFromAttribute(Attribute attribute)
-        {
-            if (attribute == null)
-            {
-                return null;
-            }
-            return (Type)attribute
-                .GetType()
-                .GetProperty("ParameterType")?
-                .GetValue(attribute);
-        }
-
-        /// <summary>
-        /// Gets the instance of <see cref="MethodInfo"/> represented by the MicrosoftSqlServerTypeMapAttribute.DbType property.
-        /// </summary>
-        /// <param name="attribute">The instance of MicrosoftSqlServerTypeMapAttribute to extract.</param>
-        /// <returns>The instance of <see cref="MethodInfo"/> represented by the MicrosoftSqlServerTypeMapAttribute.DbType property.</returns>
-        internal static MethodInfo GetMicrosoftSqlServerDbTypeFromAttributeSetMethod(Attribute attribute)
-        {
-            if (attribute == null)
-            {
-                return null;
-            }
-            return GetMicrosoftSqlServerParameterTypeFromAttribute(attribute)?
-                .GetProperty("SqlDbType")?
-                .SetMethod;
-        }
-
-        #endregion
-
-        #region MySql
-
-        /// <summary>
-        /// Gets the MySqlTypeMapAttribute if present.
-        /// </summary>
-        /// <param name="property">The instance of propery to inspect.</param>
-        /// <returns>The instance of MySqlTypeMapAttribute.</returns>
-        internal static Attribute GetMySqlDbTypeTypeMapAttribute(ClassProperty property)
-        {
-            return property?
-                .PropertyInfo
-                .GetCustomAttributes()?
-                .FirstOrDefault(e =>
-                    e.GetType().FullName.Equals("RepoDb.Attributes.MySqlTypeMapAttribute"));
-        }
-
-        /// <summary>
-        /// Gets the value represented by the MySqlTypeMapAttribute.DbType property.
-        /// </summary>
-        /// <param name="attribute">The instance of MySqlTypeMapAttribute to extract.</param>
-        /// <returns>The value represented by the MySqlTypeMapAttribute.DbType property.</returns>
-        internal static object GetMySqlDbTypeFromAttribute(Attribute attribute)
-        {
-            if (attribute == null)
-            {
-                return null;
-            }
-            var type = attribute.GetType();
-            return type
-                .GetProperty("DbType")?
-                .GetValue(attribute);
-        }
-
-        /// <summary>
-        /// Gets the system type of MySql.Data.MySqlClient.MySqlParameter represented by MySqlTypeMapAttribute.ParameterType property.
-        /// </summary>
-        /// <param name="attribute">The instance of MySqlTypeMapAttribute to extract.</param>
-        /// <returns>The type of MySql.Data.MySqlClient.MySqlParameter represented by MySqlTypeMapAttribute.ParameterType property.</returns>
-        internal static Type GetMySqlParameterTypeFromAttribute(Attribute attribute)
-        {
-            if (attribute == null)
-            {
-                return null;
-            }
-            return (Type)attribute
-                .GetType()
-                .GetProperty("ParameterType")?
-                .GetValue(attribute);
-        }
-
-        /// <summary>
-        /// Gets the instance of <see cref="MethodInfo"/> represented by the MySqlTypeMapAttribute.DbType property.
-        /// </summary>
-        /// <param name="attribute">The instance of MySqlTypeMapAttribute to extract.</param>
-        /// <returns>The instance of <see cref="MethodInfo"/> represented by the MySqlTypeMapAttribute.DbType property.</returns>
-        internal static MethodInfo GetMySqlDbTypeFromAttributeSetMethod(Attribute attribute)
-        {
-            if (attribute == null)
-            {
-                return null;
-            }
-            return GetMySqlParameterTypeFromAttribute(attribute)?
-                .GetProperty("MySqlDbType")?
-                .SetMethod;
-        }
-
-        #endregion
-
-        #region Npgsql
-
-        /// <summary>
-        /// Gets the NpgsqlDbTypeMapAttribute if present.
-        /// </summary>
-        /// <param name="property">The instance of propery to inspect.</param>
-        /// <returns>The instance of NpgsqlDbTypeMapAttribute.</returns>
-        internal static Attribute GetNpgsqlDbTypeTypeMapAttribute(ClassProperty property)
-        {
-            return property?
-                .PropertyInfo
-                .GetCustomAttributes()?
-                .FirstOrDefault(e =>
-                    e.GetType().FullName.Equals("RepoDb.Attributes.NpgsqlTypeMapAttribute"));
-        }
-
-        /// <summary>
-        /// Gets the value represented by the NpgsqlDbTypeMapAttribute.DbType property.
-        /// </summary>
-        /// <param name="attribute">The instance of NpgsqlDbTypeMapAttribute to extract.</param>
-        /// <returns>The value represented by the NpgsqlDbTypeMapAttribute.DbType property.</returns>
-        internal static object GetNpgsqlDbTypeFromAttribute(Attribute attribute)
-        {
-            if (attribute == null)
-            {
-                return null;
-            }
-            var type = attribute.GetType();
-            return type
-                .GetProperty("DbType")?
-                .GetValue(attribute);
-        }
-
-        /// <summary>
-        /// Gets the system type of NpgsqlTypes.NpgsqlParameter represented by NpgsqlDbTypeMapAttribute.ParameterType property.
-        /// </summary>
-        /// <param name="attribute">The instance of NpgsqlDbTypeMapAttribute to extract.</param>
-        /// <returns>The type of NpgsqlTypes.NpgsqlParameter represented by NpgsqlDbTypeMapAttribute.ParameterType property.</returns>
-        internal static Type GetNpgsqlParameterTypeFromAttribute(Attribute attribute)
-        {
-            if (attribute == null)
-            {
-                return null;
-            }
-            return (Type)attribute
-                .GetType()
-                .GetProperty("ParameterType")?
-                .GetValue(attribute);
-        }
-
-        /// <summary>
-        /// Gets the instance of <see cref="MethodInfo"/> represented by the NpgsqlDbTypeMapAttribute.DbType property.
-        /// </summary>
-        /// <param name="attribute">The instance of NpgsqlDbTypeMapAttribute to extract.</param>
-        /// <returns>The instance of <see cref="MethodInfo"/> represented by the NpgsqlDbTypeMapAttribute.DbType property.</returns>
-        internal static MethodInfo GetNpgsqlDbTypeFromAttributeSetMethod(Attribute attribute)
-        {
-            if (attribute == null)
-            {
-                return null;
-            }
-            return GetNpgsqlParameterTypeFromAttribute(attribute)?
-                .GetProperty("NpgsqlDbType")?
-                .SetMethod;
-        }
-
-        #endregion
-
-        #endregion
     }
 }
