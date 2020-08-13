90acd67f6ee1e1dd237b6a0929193bdc07142220
Michael Pendon <michael.c.pendon@outlook.com>
2020-08-13 16:37:41 +0200

    Added the cacher and provider for the UpdateAllExecutionContext object

---
+++ RepoDb.Core/RepoDb/Cachers/ExecutionContext/UpdateAllExecutionContextCache.cs
@@ -0,0 +1,53 @@
+﻿using RepoDb.Contexts.Execution;
+using System.Collections.Concurrent;
+
+namespace RepoDb.Cachers.ExecutionContext
+{
+    /// <summary>
+    /// A class that is used to cache the context of the UpdateAll operation.
+    /// </summary>
+    internal static class UpdateAllExecutionContextCache
+    {
+        private static ConcurrentDictionary<int, object> cache =
+            new ConcurrentDictionary<int, object>();
+
+        public static void Flush()
+        {
+            cache.Clear();
+        }
+
+        internal static void Add<TEntity>(UpdateAllExecutionContext<TEntity> context)
+            where TEntity : class
+        {
+            cache.TryAdd(context.GetHashCode(), context);
+        }
+
+
+        //internal static UpdateAllExecutionContext<TEntity> Get<TEntity>(int key)
+        //    where TEntity : class
+        //{
+        //    // Variables
+        //    var key = tableName.GetHashCode() + batchSize.GetHashCode();
+        //    var context = (UpdateAllExecutionContext<TEntity>)null;
+
+        //    // The fields hashcodes
+        //    if (fields?.Any() == true)
+        //    {
+        //        foreach (var field in fields)
+        //        {
+        //            key += field.GetHashCode();
+        //        }
+        //    }
+
+        //    // Get the cache
+        //    if (cache.TryGetValue(key, out context) == false)
+        //    {
+        //        context = callback(batchSize);
+        //        cache.TryAdd(key, context);
+        //    }
+
+        //    // Return the cache
+        //    return context;
+        //}
+    }
+}
---
+++ RepoDb.Core/RepoDb/Contexts/Providers/UpdateAllExecutionContextProvider.cs
@@ -0,0 +1,181 @@
+﻿using RepoDb.Contexts.Execution;
+using RepoDb.Extensions;
+using RepoDb.Interfaces;
+using RepoDb.Requests;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepoDb.Contexts.Providers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal static class UpdateAllExecutionContextProvider
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="connection"></param>
+        /// <param name="tableName"></param>
+        /// <param name="qualifiers"></param>
+        /// <param name="batchSize"></param>
+        /// <param name="fields"></param>
+        /// <param name="hints"></param>
+        /// <param name="transaction"></param>
+        /// <param name="statementBuilder"></param>
+        /// <returns></returns>
+        public static UpdateAllExecutionContext<TEntity> UpdateAllExecutionContext<TEntity>(IDbConnection connection,
+            string tableName,
+            IEnumerable<Field> qualifiers,
+            int batchSize,
+            IEnumerable<Field> fields,
+            string hints = null,
+            IDbTransaction transaction = null,
+            IStatementBuilder statementBuilder = null)
+            where TEntity : class
+        {
+            // Get the DB fields
+            var dbFields = DbFieldCache.Get(connection, tableName, transaction);
+
+            // Returnt the context
+            return UpdateAllExecutionContext<TEntity>(connection,
+                dbFields,
+                tableName,
+                qualifiers,
+                batchSize,
+                fields,
+                hints,
+                transaction,
+                statementBuilder);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="connection"></param>
+        /// <param name="tableName"></param>
+        /// <param name="qualifiers"></param>
+        /// <param name="batchSize"></param>
+        /// <param name="fields"></param>
+        /// <param name="hints"></param>
+        /// <param name="transaction"></param>
+        /// <param name="statementBuilder"></param>
+        /// <returns></returns>
+        public static async Task<UpdateAllExecutionContext<TEntity>> UpdateAllExecutionContextAsync<TEntity>(IDbConnection connection,
+            string tableName,
+            IEnumerable<Field> qualifiers,
+            int batchSize,
+            IEnumerable<Field> fields,
+            string hints = null,
+            IDbTransaction transaction = null,
+            IStatementBuilder statementBuilder = null)
+            where TEntity : class
+        {
+            // Get the DB fields
+            var dbFields = await DbFieldCache.GetAsync(connection, tableName, transaction);
+
+            // Returnt the context
+            return UpdateAllExecutionContext<TEntity>(connection,
+                dbFields,
+                tableName,
+                qualifiers,
+                batchSize,
+                fields,
+                hints,
+                transaction,
+                statementBuilder);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="connection"></param>
+        /// <param name="dbFields"></param>
+        /// <param name="tableName"></param>
+        /// <param name="qualifiers"></param>
+        /// <param name="batchSize"></param>
+        /// <param name="fields"></param>
+        /// <param name="hints"></param>
+        /// <param name="transaction"></param>
+        /// <param name="statementBuilder"></param>
+        /// <returns></returns>
+        public static UpdateAllExecutionContext<TEntity> UpdateAllExecutionContext<TEntity>(IDbConnection connection,
+            IEnumerable<DbField> dbFields,
+            string tableName,
+            IEnumerable<Field> qualifiers,
+            int batchSize,
+            IEnumerable<Field> fields,
+            string hints = null,
+            IDbTransaction transaction = null,
+            IStatementBuilder statementBuilder = null)
+            where TEntity : class
+        {
+            // Variables needed
+            var dbSetting = connection.GetDbSetting();
+            var inputFields = new List<DbField>();
+
+            // Check the qualifiers
+            if (qualifiers?.Any() != true)
+            {
+                var qualifier = dbFields?.FirstOrDefault(dbField => dbField.IsPrimary == true) ??
+                    dbFields?.FirstOrDefault(dbField => dbField.IsIdentity == true);
+                qualifiers = qualifier?.AsField().AsEnumerable();
+            }
+
+            // Filter the actual properties for input fields
+            inputFields = dbFields?
+                .Where(dbField =>
+                    fields.FirstOrDefault(field => string.Equals(field.Name.AsUnquoted(true, dbSetting), dbField.Name.AsUnquoted(true, dbSetting), StringComparison.OrdinalIgnoreCase)) != null)
+                .AsList();
+
+            // Variables for the context
+            var multipleEntitiesFunc = (Action<DbCommand, IList<TEntity>>)null;
+            var singleEntityFunc = (Action<DbCommand, TEntity>)null;
+
+            // Identity which objects to set
+            if (batchSize <= 1)
+            {
+                singleEntityFunc = FunctionCache.GetDataEntityDbParameterSetterCompiledFunction<TEntity>(
+                    string.Concat(typeof(TEntity).FullName, StringConstant.Period, tableName, ".UpdateAll"),
+                    inputFields?.AsList(),
+                    null,
+                    dbSetting);
+            }
+            else
+            {
+                multipleEntitiesFunc = FunctionCache.GetDataEntityListDbParameterSetterCompiledFunction<TEntity>(
+                    string.Concat(typeof(TEntity).FullName, StringConstant.Period, tableName, ".UpdateAll"),
+                    inputFields?.AsList(),
+                    null,
+                    batchSize,
+                    dbSetting);
+            }
+
+            // Identity the requests
+            var updateAllRequest = new UpdateAllRequest(tableName,
+                connection,
+                transaction,
+                fields,
+                qualifiers,
+                batchSize,
+                hints,
+                statementBuilder);
+
+            // Return the value
+            return new UpdateAllExecutionContext<TEntity>
+            {
+                CommandText = CommandTextCache.GetUpdateAllText(updateAllRequest),
+                InputFields = inputFields,
+                SingleDataEntityParametersSetterFunc = singleEntityFunc,
+                MultipleDataEntitiesParametersSetterFunc = multipleEntitiesFunc
+            };
+        }
+    }
+}
