a28138d0a1e8ccc10609f9abbce6322e2da67058
Michael Pendon <michael.c.pendon@outlook.com>
2020-08-13 16:36:22 +0200

    Added the NonKeyedTable for the Integration Tests.

---
+++ RepoDb.Core/RepoDb.Tests/RepoDb.IntegrationTests/Models/NonKeyedTable.cs
@@ -0,0 +1,12 @@
+﻿using RepoDb.Attributes;
+using System;
+
+namespace RepoDb.IntegrationTests.Models
+{
+    public class NonKeyedTable
+    {
+        public int? ColumnInt { get; set; }
+        public string ColumnNVarChar { get; set; }
+        public DateTime? ColumnDateTime2 { get; set; }
+    }
+}
