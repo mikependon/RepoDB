0909dda8a79d8585e77984d4f76d0fb20e973694
Michael Pendon <michael.c.pendon@outlook.com>
2020-08-13 16:37:00 +0200

    Added the KeyFieldNotFoundException and IdentityFieldNotFoundException classes.

---
+++ RepoDb.Core/RepoDb/Exceptions/IdentityFieldNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RepoDb.Exceptions
+{
+    /// <summary>
+    /// An exception that is being thrown if the identity key is not found from the data entity.
+    /// </summary>
+    public class IdentityFieldNotFoundException : Exception
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="IdentityFieldNotFoundException"/> class.
+        /// </summary>
+        /// <param name="message">The exception message.</param>
+        public IdentityFieldNotFoundException(string message)
+            : base(message) { }
+    }
+}
---
+++ RepoDb.Core/RepoDb/Exceptions/KeyFieldNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RepoDb.Exceptions
+{
+    /// <summary>
+    /// An exception that is being thrown if the primary key and identity key is not found from the data entity.
+    /// </summary>
+    public class KeyFieldNotFoundException : Exception
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="KeyFieldNotFoundException"/> class.
+        /// </summary>
+        /// <param name="message">The exception message.</param>
+        public KeyFieldNotFoundException(string message)
+            : base(message) { }
+    }
+}
