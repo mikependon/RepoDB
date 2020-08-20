741e4b3aeb01b64b07115dd5a6ea3d44f58bfe13
Michael Pendon <michael.c.pendon@outlook.com>
2020-08-20 07:54:46 +0200

    Ensure that BaseRepository is only catering single entity.

--- RepoDb.Core/RepoDb/Operations/BaseRepository/Insert.cs
+++ RepoDb.Core/RepoDb/Operations/BaseRepository/Insert.cs
@@ -14,45 +14,6 @@ namespace RepoDb
         /// <summary>
         /// Inserts a new row in the table.
         /// </summary>
-        /// <param name="tableName">The name of the target table to be used.</param>
-        /// <param name="entity">The data entity object to be inserted.</param>
-        /// <param name="hints">The table hints to be used.</param>
-        /// <param name="transaction">The transaction to be used.</param>
-        /// <returns>The value of the identity field if present, otherwise, the value of the primary field.</returns>
-        public object Insert(string tableName,
-            TEntity entity,
-            string hints = null,
-            IDbTransaction transaction = null)
-        {
-            return DbRepository.Insert<TEntity>(tableName: tableName,
-                entity: entity,
-                hints: hints,
-                transaction: transaction);
-        }
-
-        /// <summary>
-        /// Inserts a new row in the table.
-        /// </summary>
-        /// <typeparam name="TResult">The target type of the result.</typeparam>
-        /// <param name="tableName">The name of the target table to be used.</param>
-        /// <param name="entity">The data entity object to be inserted.</param>
-        /// <param name="hints">The table hints to be used.</param>
-		/// <param name="transaction">The transaction to be used.</param>
-        /// <returns>The value of the identity field if present, otherwise, the value of the primary field.</returns>
-        public TResult Insert<TResult>(string tableName,
-            TEntity entity,
-            string hints = null,
-            IDbTransaction transaction = null)
-        {
-            return DbRepository.Insert<TEntity, TResult>(tableName: tableName,
-                entity: entity,
-                hints: hints,
-                transaction: transaction);
-        }
-
-        /// <summary>
-        /// Inserts a new row in the table.
-        /// </summary>
         /// <param name="entity">The data entity object to be inserted.</param>
         /// <param name="hints">The table hints to be used.</param>
 		/// <param name="transaction">The transaction to be used.</param>
@@ -86,45 +47,6 @@ namespace RepoDb
         #endregion
 
         #region InsertAsync<TEntity>
-
-        /// <summary>
-        /// Inserts a new row in the table in an asynchronous way.
-        /// </summary>
-        /// <param name="tableName">The name of the target table to be used.</param>
-        /// <param name="entity">The data entity object to be inserted.</param>
-        /// <param name="hints">The table hints to be used.</param>
-        /// <param name="transaction">The transaction to be used.</param>
-        /// <returns>The value of the identity field if present, otherwise, the value of the primary field.</returns>
-        public Task<object> InsertAsync(string tableName,
-            TEntity entity,
-            string hints = null,
-            IDbTransaction transaction = null)
-        {
-            return DbRepository.InsertAsync<TEntity>(tableName: tableName,
-                entity: entity,
-                hints: hints,
-                transaction: transaction);
-        }
-
-        /// <summary>
-        /// Inserts a new row in the table in an asynchronous way.
-        /// </summary>
-        /// <typeparam name="TResult">The target type of the result.</typeparam>
-        /// <param name="tableName">The name of the target table to be used.</param>
-        /// <param name="entity">The data entity object to be inserted.</param>
-        /// <param name="hints">The table hints to be used.</param>
-		/// <param name="transaction">The transaction to be used.</param>
-        /// <returns>The value of the identity field if present, otherwise, the value of the primary field.</returns>
-        public Task<TResult> InsertAsync<TResult>(string tableName,
-            TEntity entity,
-            string hints = null,
-            IDbTransaction transaction = null)
-        {
-            return DbRepository.InsertAsync<TEntity, TResult>(tableName: tableName,
-                entity: entity,
-                hints: hints,
-                transaction: transaction);
-        }
 
         /// <summary>
         /// Inserts a new row in the table in an asynchronous way.
--- RepoDb.Core/RepoDb/Operations/BaseRepository/InsertAll.cs
+++ RepoDb.Core/RepoDb/Operations/BaseRepository/InsertAll.cs
@@ -15,31 +15,6 @@ namespace RepoDb
         /// <summary>
         /// Insert multiple rows in the table.
         /// </summary>
-        /// <param name="tableName">The name of the target table to be used.</param>
-        /// <param name="entities">The data entity objects to be inserted.</param>
-        /// <param name="batchSize">The batch size of the insertion.</param>
-        /// <param name="fields">The mapping list of <see cref="Field"/> objects to be used.</param>
-        /// <param name="hints">The table hints to be used.</param>
-        /// <param name="transaction">The transaction to be used.</param>
-        /// <returns>The number of inserted rows in the table.</returns>
-        public int InsertAll(string tableName,
-            IEnumerable<TEntity> entities,
-            int batchSize = Constant.DefaultBatchOperationSize,
-            IEnumerable<Field> fields = null,
-            string hints = null,
-            IDbTransaction transaction = null)
-        {
-            return DbRepository.InsertAll<TEntity>(tableName: tableName,
-                entities: entities,
-                batchSize: batchSize,
-                fields: fields,
-                hints: hints,
-                transaction: transaction);
-        }
-
-        /// <summary>
-        /// Insert multiple rows in the table.
-        /// </summary>
         /// <param name="entities">The data entity objects to be inserted.</param>
         /// <param name="batchSize">The batch size of the insertion.</param>
         /// <param name="hints">The table hints to be used.</param>
@@ -59,31 +34,6 @@ namespace RepoDb
         #endregion
 
         #region InsertAllAsync<TEntity>
-
-        /// <summary>
-        /// Insert multiple rows in the table in an asynchronous way.
-        /// </summary>
-        /// <param name="tableName">The name of the target table to be used.</param>
-        /// <param name="entities">The data entity objects to be inserted.</param>
-        /// <param name="batchSize">The batch size of the insertion.</param>
-        /// <param name="fields">The mapping list of <see cref="Field"/> objects to be used.</param>
-        /// <param name="hints">The table hints to be used.</param>
-        /// <param name="transaction">The transaction to be used.</param>
-        /// <returns>The number of inserted rows in the table.</returns>
-        public Task<int> InsertAllAsync(string tableName,
-            IEnumerable<TEntity> entities,
-            int batchSize = Constant.DefaultBatchOperationSize,
-            IEnumerable<Field> fields = null,
-            string hints = null,
-            IDbTransaction transaction = null)
-        {
-            return DbRepository.InsertAllAsync<TEntity>(tableName: tableName,
-                entities: entities,
-                batchSize: batchSize,
-                fields: fields,
-                hints: hints,
-                transaction: transaction);
-        }
 
         /// <summary>
         /// Insert multiple rows in the table in an asynchronous way.
