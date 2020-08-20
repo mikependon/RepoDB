813f4acca402ca002b06ebb2d870540ba9b9098d
Michael Pendon <michael.c.pendon@outlook.com>
2020-08-20 15:43:44 +0200

    Added a new exception for the missing qualifiers.

---
+++ RepoDb.Core/RepoDb/Exceptions/MissingQualifierFieldsException.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace RepoDb.Exceptions
+{
+    /// <summary>
+    /// An exception that is being thrown if the qualifier fields are not found from the request.
+    /// </summary>
+    public class MissingQualifierFieldsException : Exception
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="MissingQualifierFieldsException"/> class.
+        /// </summary>
+        /// <param name="message">The exception message.</param>
+        public MissingQualifierFieldsException(string message)
+            : base(message) { }
+    }
+}
