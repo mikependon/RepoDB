d9439403fc3125104fd81a2c54848c5ea98cc2e1
Michael Pendon <michael.c.pendon@outlook.com>
2020-08-27 15:22:23 +0200

    Added integration tests for Insert, Merge and Update operations. #524

--- RepoDb.Core/RepoDb.Tests/RepoDb.IntegrationTests/ClassHandlerTest.cs
+++ RepoDb.Core/RepoDb.Tests/RepoDb.IntegrationTests/ClassHandlerTest.cs
@@ -10,7 +10,6 @@ using System.Linq;
 using RepoDb.IntegrationTests.Models;
 using System.Data.Common;
 using RepoDb.Enumerations;
-using System.Reflection;
 
 namespace RepoDb.IntegrationTests
 {
@@ -34,27 +33,27 @@ namespace RepoDb.IntegrationTests
 
         private class ClassHandlerIdentityTableClassHandler : IClassHandler<ClassHandlerIdentityTable>
         {
-            public int GetCallCount { get; set; }
+            public int GetMethodCallCount { get; set; }
 
-            public int SetCallCount { get; set; }
+            public int SetMethodCallCount { get; set; }
 
             public ClassHandlerIdentityTable Get(ClassHandlerIdentityTable entity,
                 DbDataReader reader)
             {
-                ++GetCallCount;
+                ++GetMethodCallCount;
                 return entity;
             }
 
             public ClassHandlerIdentityTable Set(ClassHandlerIdentityTable entity)
             {
-                ++SetCallCount;
+                ++SetMethodCallCount;
                 return entity;
             }
 
             public void Reset()
             {
-                GetCallCount = 0;
-                SetCallCount = 0;
+                GetMethodCallCount = 0;
+                SetMethodCallCount = 0;
             }
         }
 
@@ -116,7 +115,7 @@ namespace RepoDb.IntegrationTests
                     where: (object)null);
 
                 // Assert
-                Assert.AreEqual(tables.Count, handler.GetCallCount);
+                Assert.AreEqual(tables.Count, handler.GetMethodCallCount);
                 Assert.AreEqual(tables.Count, result.Count());
                 result.AsList().ForEach(item =>
                 {
@@ -149,13 +148,149 @@ namespace RepoDb.IntegrationTests
                 var result = connection.ExecuteQuery<ClassHandlerIdentityTable>("SELECT * FROM [sc].[IdentityTable];");
 
                 // Assert
-                Assert.AreEqual(tables.Count, handler.GetCallCount);
+                Assert.AreEqual(tables.Count, handler.GetMethodCallCount);
                 Assert.AreEqual(tables.Count, result.Count());
                 result.AsList().ForEach(item =>
                 {
                     var target = tables.First(t => t.Id == item.Id);
                     Helper.AssertPropertiesEquality(target, item);
                 });
+            }
+        }
+
+        #endregion
+
+        #region Merge
+
+        [TestMethod]
+        public void TestMergeWithClassHandler()
+        {
+            // Setup
+            var table = CreateClassHandlerIdentityTables(1).First();
+
+            using (var connection = new SqlConnection(Database.ConnectionStringForRepoDb))
+            {
+                // Setup
+                var handler = ClassHandlerCache.Get<ClassHandlerIdentityTableClassHandler>(typeof(ClassHandlerIdentityTable));
+                handler.Reset();
+
+                // Act
+                var id = connection.Merge(table);
+
+                // Assert
+                Assert.AreEqual(1, handler.SetMethodCallCount);
+            }
+        }
+
+        [TestMethod]
+        public void TestMergeManyWithClassHandler()
+        {
+            // Setup
+            var tables = CreateClassHandlerIdentityTables(10).AsList();
+
+            using (var connection = new SqlConnection(Database.ConnectionStringForRepoDb))
+            {
+                // Setup
+                var handler = ClassHandlerCache.Get<ClassHandlerIdentityTableClassHandler>(typeof(ClassHandlerIdentityTable));
+                handler.Reset();
+
+                // Act
+                tables.ForEach(table => connection.Merge(table));
+
+                // Assert
+                Assert.AreEqual(tables.Count, handler.SetMethodCallCount);
+            }
+        }
+
+        #endregion
+
+        #region MergeAll
+
+        [TestMethod]
+        public void TestMergeAllWithClassHandler()
+        {
+            // Setup
+            var tables = CreateClassHandlerIdentityTables(10).AsList();
+
+            using (var connection = new SqlConnection(Database.ConnectionStringForRepoDb))
+            {
+                // Setup
+                var handler = ClassHandlerCache.Get<ClassHandlerIdentityTableClassHandler>(typeof(ClassHandlerIdentityTable));
+                handler.Reset();
+
+                // Act
+                connection.MergeAll(tables);
+
+                // Assert
+                Assert.AreEqual(tables.Count, handler.SetMethodCallCount);
+            }
+        }
+
+        #endregion
+
+        #region Insert
+
+        [TestMethod]
+        public void TestInsertWithClassHandler()
+        {
+            // Setup
+            var table = CreateClassHandlerIdentityTables(1).First();
+
+            using (var connection = new SqlConnection(Database.ConnectionStringForRepoDb))
+            {
+                // Setup
+                var handler = ClassHandlerCache.Get<ClassHandlerIdentityTableClassHandler>(typeof(ClassHandlerIdentityTable));
+                handler.Reset();
+
+                // Act
+                var id = connection.Insert(table);
+
+                // Assert
+                Assert.AreEqual(1, handler.SetMethodCallCount);
+            }
+        }
+
+        [TestMethod]
+        public void TestInsertManyWithClassHandler()
+        {
+            // Setup
+            var tables = CreateClassHandlerIdentityTables(10).AsList();
+
+            using (var connection = new SqlConnection(Database.ConnectionStringForRepoDb))
+            {
+                // Setup
+                var handler = ClassHandlerCache.Get<ClassHandlerIdentityTableClassHandler>(typeof(ClassHandlerIdentityTable));
+                handler.Reset();
+
+                // Act
+                tables.ForEach(table => connection.Insert(table));
+
+                // Assert
+                Assert.AreEqual(tables.Count, handler.SetMethodCallCount);
+            }
+        }
+
+        #endregion
+
+        #region InsertAll
+
+        [TestMethod]
+        public void TestInsertAllWithClassHandler()
+        {
+            // Setup
+            var tables = CreateClassHandlerIdentityTables(10).AsList();
+
+            using (var connection = new SqlConnection(Database.ConnectionStringForRepoDb))
+            {
+                // Setup
+                var handler = ClassHandlerCache.Get<ClassHandlerIdentityTableClassHandler>(typeof(ClassHandlerIdentityTable));
+                handler.Reset();
+
+                // Act
+                connection.InsertAll(tables);
+
+                // Assert
+                Assert.AreEqual(tables.Count, handler.SetMethodCallCount);
             }
         }
 
@@ -182,7 +317,7 @@ namespace RepoDb.IntegrationTests
                 var result = connection.Query<ClassHandlerIdentityTable>(id).First();
 
                 // Assert
-                Assert.AreEqual(1, handler.GetCallCount);
+                Assert.AreEqual(1, handler.GetMethodCallCount);
                 Helper.AssertPropertiesEquality(table, result);
             }
         }
@@ -210,13 +345,90 @@ namespace RepoDb.IntegrationTests
                 var result = connection.QueryAll<ClassHandlerIdentityTable>();
 
                 // Assert
-                Assert.AreEqual(tables.Count, handler.GetCallCount);
+                Assert.AreEqual(tables.Count, handler.GetMethodCallCount);
                 Assert.AreEqual(tables.Count, result.Count());
                 result.AsList().ForEach(item =>
                 {
                     var target = tables.First(t => t.Id == item.Id);
                     Helper.AssertPropertiesEquality(target, item);
                 });
+            }
+        }
+
+        #endregion
+
+        #region Update
+
+        [TestMethod]
+        public void TestUpdateWithClassHandler()
+        {
+            // Setup
+            var table = CreateClassHandlerIdentityTables(1).First();
+
+            using (var connection = new SqlConnection(Database.ConnectionStringForRepoDb))
+            {
+                // Act
+                connection.Insert(table);
+
+                // Setup
+                var handler = ClassHandlerCache.Get<ClassHandlerIdentityTableClassHandler>(typeof(ClassHandlerIdentityTable));
+                handler.Reset();
+
+                // Act
+                connection.Update(table);
+
+                // Assert
+                Assert.AreEqual(1, handler.SetMethodCallCount);
+            }
+        }
+
+        [TestMethod]
+        public void TestUpdateManyWithClassHandler()
+        {
+            // Setup
+            var tables = CreateClassHandlerIdentityTables(10).AsList();
+
+            using (var connection = new SqlConnection(Database.ConnectionStringForRepoDb))
+            {
+                // Act
+                connection.InsertAll(tables);
+
+                // Setup
+                var handler = ClassHandlerCache.Get<ClassHandlerIdentityTableClassHandler>(typeof(ClassHandlerIdentityTable));
+                handler.Reset();
+
+                // Act
+                tables.ForEach(table => connection.Update(table));
+
+                // Assert
+                Assert.AreEqual(tables.Count, handler.SetMethodCallCount);
+            }
+        }
+
+        #endregion
+
+        #region UpdateAll
+
+        [TestMethod]
+        public void TestUpdateAllWithClassHandler()
+        {
+            // Setup
+            var tables = CreateClassHandlerIdentityTables(10).AsList();
+
+            using (var connection = new SqlConnection(Database.ConnectionStringForRepoDb))
+            {
+                // Act
+                connection.InsertAll(tables);
+
+                // Setup
+                var handler = ClassHandlerCache.Get<ClassHandlerIdentityTableClassHandler>(typeof(ClassHandlerIdentityTable));
+                handler.Reset();
+
+                // Act
+                connection.UpdateAll(tables);
+
+                // Assert
+                Assert.AreEqual(tables.Count, handler.SetMethodCallCount);
             }
         }
 
