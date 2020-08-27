765e82d2f5c541f84e888af18f50c92765ea8f63
Michael Pendon <michael.c.pendon@outlook.com>
2020-08-27 07:16:23 +0200

    Added the initial classes needed for the Class Handler.#524

---
+++ RepoDb.Core/RepoDb/Attributes/ClassHandlerAttribute.cs
@@ -0,0 +1,47 @@
+﻿using RepoDb.Exceptions;
+using RepoDb.Extensions;
+using System;
+
+namespace RepoDb.Attributes
+{
+    /// <summary>
+    /// An attribute that is used to define a handler for the property transformation.
+    /// </summary>
+    public class ClassHandlerAttribute : Attribute
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="ClassHandlerAttribute"/> class.
+        /// </summary>
+        /// <param name="handlerType">The type of the handler.</param>
+        public ClassHandlerAttribute(Type handlerType)
+        {
+            Validate(handlerType);
+            HandlerType = handlerType;
+        }
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the type of the handler that is being used.
+        /// </summary>
+        public Type HandlerType { get; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="handlerType"></param>
+        private void Validate(Type handlerType)
+        {
+            if (handlerType?.IsInterfacedTo(StaticType.IClassHandler) != true)
+            {
+                throw new InvalidTypeException($"Type '{handlerType.FullName}' must implement the '{StaticType.IClassHandler}' interface.");
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ RepoDb.Core/RepoDb/Interfaces/IClassHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Common;
+
+namespace RepoDb.Interfaces
+{
+    /// <summary>
+    /// An interface that is used to mark a class to be a class handler.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the data entity.</typeparam>
+    public interface IClassHandler<TEntity>
+        where TEntity : class
+    {
+        /// <summary>
+        /// The method that is being invoked when the outbound transformation is triggered (ie: Query).
+        /// </summary>
+        /// <param name="entity">The current instance of data entity object.</param>
+        /// <param name="reader">The instance of <see cref="DbDataReader"/> in used to populate the data entity object.</param>
+        /// <returns>The current or the newly created instance data entity object.</returns>
+        TEntity Get(TEntity entity,
+            DbDataReader reader);
+
+        /// <summary>
+        /// The method that is being invoked when the inbound transformation is triggered (ie: Insert, Update, Merge).
+        /// </summary>
+        /// <param name="entity">The current instance of data entity object.</param>
+        /// <returns>The current or the newly created instance data entity object.</returns>
+        TEntity Set(TEntity entity);
+    }
+}
