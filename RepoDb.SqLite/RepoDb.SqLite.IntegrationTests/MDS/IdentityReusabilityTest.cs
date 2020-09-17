6886bffe16c1d073a09618606ec8db8a6489502a
Michael Pendon <michael.c.pendon@outlook.com>
2020-09-17 16:36:22 +0200

    #568 - Added the Integration Tests for reusable identity value.

---
+++ RepoDb.SqLite/RepoDb.SqLite.IntegrationTests/MDS/IdentityReusabilityTest.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoDb.Extensions;
+using RepoDb.SqLite.IntegrationTests.Models;
+using RepoDb.SqLite.IntegrationTests.Setup;
+using System.Linq;
+
+namespace RepoDb.SqLite.IntegrationTests.MDS
+{
+    [TestClass]
+    public class IdentityReusabilityTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            Database.Initialize();
+            Cleanup();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Database.Cleanup();
+        }
+
+        [TestMethod]
+        public void TestSqLiteConnectionInsertForIdentityReusability()
+        {
+            using (var connection = new SqliteConnection(Database.ConnectionStringMDS))
+            {
+                // Create the tables
+                Database.CreateMdsTables(connection);
+
+                // Setup
+                var tables = Helper.CreateMdsCompleteTables(10).AsList();
+
+                // Act
+                var insertAllResult = connection.InsertAll<MdsCompleteTable>(tables);
+
+                // Assert
+                Assert.AreEqual(tables.Count, insertAllResult);
+                Assert.AreEqual(tables.Count, connection.CountAll<MdsCompleteTable>());
+
+                // Setup (3)
+                var deleteEntity = tables[2];
+
+                // Act (3)
+                var deleteResult = connection.Delete<MdsCompleteTable>(deleteEntity);
+
+                // Assert
+                Assert.AreEqual(1, deleteResult);
+                Assert.AreEqual(tables.Count - 1, connection.CountAll<MdsCompleteTable>());
+
+                // Setup
+                var table = Helper.CreateMdsCompleteTables(1).First();
+
+                // Act (3)
+                var fields = FieldCache.Get<MdsCompleteTable>().Where(e => e.Name != "Id");
+                var insertResult = connection.Insert<MdsCompleteTable>(table, fields: fields);
+
+                // Assert (3)
+                Assert.AreEqual(deleteEntity.Id, insertResult);
+            }
+        }
+
+        [TestMethod]
+        public void TestSqLiteConnectionInsertAsyncForIdentityReusability()
+        {
+            using (var connection = new SqliteConnection(Database.ConnectionStringMDS))
+            {
+                // Create the tables
+                Database.CreateMdsTables(connection);
+
+                // Setup
+                var tables = Helper.CreateMdsCompleteTables(10).AsList();
+
+                // Act
+                var insertAllResult = connection.InsertAll<MdsCompleteTable>(tables);
+
+                // Assert
+                Assert.AreEqual(tables.Count, insertAllResult);
+                Assert.AreEqual(tables.Count, connection.CountAll<MdsCompleteTable>());
+
+                // Setup (3)
+                var deleteEntity = tables[2];
+
+                // Act (3)
+                var deleteResult = connection.Delete<MdsCompleteTable>(deleteEntity);
+
+                // Assert
+                Assert.AreEqual(1, deleteResult);
+                Assert.AreEqual(tables.Count - 1, connection.CountAll<MdsCompleteTable>());
+
+                // Setup
+                var table = Helper.CreateMdsCompleteTables(1).First();
+
+                // Act (3)
+                var fields = FieldCache.Get<MdsCompleteTable>().Where(e => e.Name != "Id");
+                var insertResult = connection.InsertAsync<MdsCompleteTable>(table, fields: fields).Result;
+
+                // Assert (3)
+                Assert.AreEqual(deleteEntity.Id, insertResult);
+            }
+        }
+    }
+}
---
+++ RepoDb.SqLite/RepoDb.SqLite.IntegrationTests/SDS/IdentityReusabilityTest.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoDb.Extensions;
+using RepoDb.SqLite.IntegrationTests.Models;
+using RepoDb.SqLite.IntegrationTests.Setup;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace RepoDb.SqLite.IntegrationTests.SDS
+{
+    [TestClass]
+    public class IdentityReusabilityTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            Database.Initialize();
+            Cleanup();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Database.Cleanup();
+        }
+
+        [TestMethod]
+        public void TestSQLiteConnectionInsertForIdentityReusability()
+        {
+            using (var connection = new SQLiteConnection(Database.ConnectionStringSDS))
+            {
+                // Create the tables
+                Database.CreateSdsTables(connection);
+
+                // Setup
+                var tables = Helper.CreateSdsCompleteTables(10).AsList();
+
+                // Act
+                var insertAllResult = connection.InsertAll<SdsCompleteTable>(tables);
+
+                // Assert
+                Assert.AreEqual(tables.Count, insertAllResult);
+                Assert.AreEqual(tables.Count, connection.CountAll<SdsCompleteTable>());
+
+                // Setup (3)
+                var deleteEntity = tables[2];
+
+                // Act (3)
+                var deleteResult = connection.Delete<SdsCompleteTable>(deleteEntity);
+
+                // Assert
+                Assert.AreEqual(1, deleteResult);
+                Assert.AreEqual(tables.Count - 1, connection.CountAll<SdsCompleteTable>());
+
+                // Setup
+                var table = Helper.CreateSdsCompleteTables(1).First();
+
+                // Act (3)
+                var fields = FieldCache.Get<SdsCompleteTable>().Where(e => e.Name != "Id");
+                var insertResult = connection.Insert<SdsCompleteTable>(table, fields: fields);
+
+                // Assert (3)
+                Assert.AreEqual(deleteEntity.Id, insertResult);
+            }
+        }
+
+        [TestMethod]
+        public void TestSQLiteConnectionInsertAsyncForIdentityReusability()
+        {
+            using (var connection = new SQLiteConnection(Database.ConnectionStringSDS))
+            {
+                // Create the tables
+                Database.CreateSdsTables(connection);
+
+                // Setup
+                var tables = Helper.CreateSdsCompleteTables(10).AsList();
+
+                // Act
+                var insertAllResult = connection.InsertAll<SdsCompleteTable>(tables);
+
+                // Assert
+                Assert.AreEqual(tables.Count, insertAllResult);
+                Assert.AreEqual(tables.Count, connection.CountAll<SdsCompleteTable>());
+
+                // Setup (3)
+                var deleteEntity = tables[2];
+
+                // Act (3)
+                var deleteResult = connection.Delete<SdsCompleteTable>(deleteEntity);
+
+                // Assert
+                Assert.AreEqual(1, deleteResult);
+                Assert.AreEqual(tables.Count - 1, connection.CountAll<SdsCompleteTable>());
+
+                // Setup
+                var table = Helper.CreateSdsCompleteTables(1).First();
+
+                // Act (3)
+                var fields = FieldCache.Get<SdsCompleteTable>().Where(e => e.Name != "Id");
+                var insertResult = connection.InsertAsync<SdsCompleteTable>(table, fields: fields).Result;
+
+                // Assert (3)
+                Assert.AreEqual(deleteEntity.Id, insertResult);
+            }
+        }
+    }
+}
