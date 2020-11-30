80837974e9a236f300a487e85c98468ffc5d8dd0
Michael Pendon <michael.c.pendon@outlook.com>
2020-11-30 21:36:45 +0100

    #650 Recommended: Exposed the IsPlainType() and IsQueryObject() extension methods.

---
+++ RepoDb.Core/RepoDb.Tests/RepoDb.UnitTests/Extensions/DbCommandExtensionsTest - Copy.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoDb.Extensions;
+using System.Linq;
+
+namespace RepoDb.UnitTests.Extensions
+{
+    [TestClass]
+    public class TypeExtensionsTest
+    {
+        [TestMethod]
+        public void TestIsPlainTypeForAnoynmousType()
+        {
+            var type = (new { Property = "ABC" }).GetType();
+            Assert.IsTrue(type.IsClassType() || type.IsAnonymousType());
+            Assert.IsFalse(type.IsQueryObjectType());
+            Assert.IsFalse(type.IsDictionaryStringObject());
+            Assert.IsFalse(type.GetEnumerableClassProperties().Any());
+        }
+    }
+}
