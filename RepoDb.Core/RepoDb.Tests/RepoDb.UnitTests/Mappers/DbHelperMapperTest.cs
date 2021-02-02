99e6dd90f7bacb6708cc9c8362b6387172fcb3f1
Tommaso Bertoni <TBertoni@artiso.com>
2021-02-02 15:34:18 +0100

    Adds tests for generic TDbConnection mappings.

---
+++ RepoDb.Core/RepoDb.Tests/RepoDb.UnitTests/Mappers/DbHelperMapperTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoDb.UnitTests.CustomObjects;
+
+namespace RepoDb.UnitTests.Mappers
+{
+    [TestClass]
+    public partial class DbHelperMapperTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            Cleanup();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            DbHelperMapper.Clear();
+        }
+
+        #region Methods
+
+        [TestMethod]
+        public void TestDbHelperMapperMappingViaGeneric()
+        {
+            // Setup
+            var dbHelper = new CustomDbHelper();
+            DbHelperMapper.Add<CustomDbConnection>(dbHelper, true);
+
+            // Act
+            var actual = DbHelperMapper.Get<CustomDbConnection>();
+
+            // Assert
+            Assert.AreSame(dbHelper, actual);
+        }
+
+        [TestMethod]
+        public void TestDbHelperMapperMappingCanBeRemovedViaGeneric()
+        {
+            // Setup
+            var dbHelper = new CustomDbHelper();
+            DbHelperMapper.Add<CustomDbConnection>(dbHelper, true);
+
+            // Act
+            DbHelperMapper.Remove<CustomDbConnection>();
+
+            // Assert
+            var actual = DbHelperMapper.Get<CustomDbConnection>();
+            Assert.IsNull(actual);
+        }
+
+        #endregion
+    }
+}
---
+++ RepoDb.Core/RepoDb.Tests/RepoDb.UnitTests/Mappers/DbSettingMapperTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoDb.UnitTests.CustomObjects;
+
+namespace RepoDb.UnitTests.Mappers
+{
+    [TestClass]
+    public partial class DbSettingMapperTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            Cleanup();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            DbSettingMapper.Clear();
+        }
+
+        #region Methods
+
+        [TestMethod]
+        public void TestDbSettingMapperMappingViaGeneric()
+        {
+            // Setup
+            var dbSetting = new CustomDbSetting();
+            DbSettingMapper.Add<CustomDbConnection>(dbSetting, true);
+
+            // Act
+            var actual = DbSettingMapper.Get<CustomDbConnection>();
+
+            // Assert
+            Assert.AreSame(dbSetting, actual);
+        }
+
+        [TestMethod]
+        public void TestDbSettingMapperMappingCanBeRemovedViaGeneric()
+        {
+            // Setup
+            var dbSetting = new CustomDbSetting();
+            DbSettingMapper.Add<CustomDbConnection>(dbSetting, true);
+
+            // Act
+            DbSettingMapper.Remove<CustomDbConnection>();
+
+            // Assert
+            var actual = DbSettingMapper.Get<CustomDbConnection>();
+            Assert.IsNull(actual);
+        }
+
+        #endregion
+    }
+}
