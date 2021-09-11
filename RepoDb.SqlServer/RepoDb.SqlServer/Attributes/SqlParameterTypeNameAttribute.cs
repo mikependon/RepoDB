edafa5a1b1fcdcbd4d1fa54766addd613715ef1c
Michael Pendon <michael.c.pendon@outlook.com>
2021-09-11 23:19:08 +0200

    #857 - Added the necessary attributes for the MDS.

---
+++ RepoDb.SqlServer/RepoDb.SqlServer/Attributes/SqlParameterTypeNameAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace RepoDb.Attributes
+{
+    /// <summary>
+    /// An attribute used to define a value to the <see cref="SqlParameter.TypeName"/> property via an entity property
+    /// before the actual execution.
+    /// </summary>
+    public class SqlParameterTypeNameAttribute : ParameterPropertyValueSetterAttribute
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="SqlParameterTypeNameAttribute"/> class.
+        /// </summary>
+        /// <param name="typeName">The name of the type.</param>
+        public SqlParameterTypeNameAttribute(string typeName)
+            : base(typeof(SqlParameter), nameof(SqlParameter.TypeName), typeName)
+        { }
+
+        /// <summary>
+        /// Gets a type name that is currently mapped.
+        /// </summary>
+        public string TypeName => (string)Value;
+    }
+}
---
+++ RepoDb.SqlServer/RepoDb.SqlServer/Attributes/SqlParameterUdtTypeNameAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace RepoDb.Attributes
+{
+    /// <summary>
+    /// An attribute used to define a value to the <see cref="SqlParameter.UdtTypeName"/> property via an entity property
+    /// before the actual execution.
+    /// </summary>
+    public class SqlParameterUdtTypeNameAttribute : ParameterPropertyValueSetterAttribute
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="SqlParameterTypeNameAttribute"/> class.
+        /// </summary>
+        /// <param name="udtTypeName">The name of the UTD.</param>
+        public SqlParameterUdtTypeNameAttribute(string udtTypeName)
+            : base(typeof(SqlParameter), nameof(SqlParameter.UdtTypeName), udtTypeName)
+        { }
+
+        /// <summary>
+        /// Gets a type name that is currently mapped.
+        /// </summary>
+        public string UdtTypeName => (string)Value;
+    }
+}
