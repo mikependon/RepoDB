92549f76174da0a9b610135e9be3441457ce91ee
Michael Pendon <michael.c.pendon@outlook.com>
2021-09-14 00:21:55 +0200

    Minor renames on the newly created attributes.

--- RepoDb.SqLite/RepoDb.SqLite/Attributes/SqliteParameterTypeAttribute.cs
+++ RepoDb.SqLite/RepoDb.SqLite/Attributes/SqliteParameterTypeAttribute.cs
@@ -6,13 +6,13 @@ namespace RepoDb.Attributes
     /// An attribute used to define a value to the <see cref="SqliteParameter.SqliteType"/>
     /// property via an entity property before the actual execution.
     /// </summary>
-    public class SqliteParameterSqliteTypeAttribute : ParameterPropertyValueSetterAttribute
+    public class SqliteParameterTypeAttribute : ParameterPropertyValueSetterAttribute
     {
         /// <summary>
-        /// Creates a new instance of <see cref="SqliteParameterSqliteTypeAttribute"/> class.
+        /// Creates a new instance of <see cref="SqliteParameterTypeAttribute"/> class.
         /// </summary>
         /// <param name="sqliteType">A target <see cref="Microsoft.Data.Sqlite.SqliteType"/> value.</param>
-        public SqliteParameterSqliteTypeAttribute(SqliteType sqliteType)
+        public SqliteParameterTypeAttribute(SqliteType sqliteType)
             : base(typeof(SqliteParameter), nameof(SqliteParameter.SqliteType), sqliteType)
         { }
 
