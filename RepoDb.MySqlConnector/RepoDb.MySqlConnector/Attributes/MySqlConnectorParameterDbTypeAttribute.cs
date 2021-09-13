c34e5ae4b6347ecd243680bcbf872cbcc1c00315
Michael Pendon <michael.c.pendon@outlook.com>
2021-09-13 12:14:40 +0200

    #875 Added the new attributes that would deprecate the existing one.

---
+++ RepoDb.MySqlConnector/RepoDb.MySqlConnector/Attributes/MySqlConnectorParameterDbTypeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using MySqlConnector;
+
+namespace RepoDb.Attributes
+{
+    /// <summary>
+    /// An attribute used to define a value to the <see cref="MySqlParameter.MySqlDbType"/>
+    /// property via an entity property before the actual execution.
+    /// </summary>
+    public class MySqlConnectorParameterDbTypeAttribute : ParameterPropertyValueSetterAttribute
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="MySqlConnectorTypeMapAttribute"/> class.
+        /// </summary>
+        /// <param name="mySqlDbType">A target <see cref="MySqlConnector.MySqlDbType"/> value.</param>
+        public MySqlConnectorParameterDbTypeAttribute(MySqlDbType mySqlDbType)
+            : base(typeof(MySqlParameter), nameof(MySqlParameter.MySqlDbType), mySqlDbType)
+        { }
+
+        /// <summary>
+        /// Gets a <see cref="MySqlConnector.MySqlDbType"/> that is currently mapped.
+        /// </summary>
+        public MySqlDbType MySqlDbType => (MySqlDbType)Value;
+    }
+}
---
+++ RepoDb.PostgreSql/RepoDb.PostgreSql/Attributes/NpgsqlParameterDbTypeAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Npgsql;
+using NpgsqlTypes;
+
+namespace RepoDb.Attributes
+{
+    /// <summary>
+    /// An attribute used to define a value to the <see cref="NpgsqlParameter.NpgsqlDbType"/>
+    /// property via an entity property before the actual execution.
+    /// </summary>
+    public class NpgsqlParameterDbTypeAttribute : ParameterPropertyValueSetterAttribute
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="NpgsqlTypeMapAttribute"/> class.
+        /// </summary>
+        /// <param name="npgsqlDbType">A target <see cref="NpgsqlTypes.NpgsqlDbType"/> value.</param>
+        public NpgsqlParameterDbTypeAttribute(NpgsqlDbType npgsqlDbType)
+            : base(typeof(NpgsqlParameter), nameof(NpgsqlParameter.NpgsqlDbType), npgsqlDbType)
+        { }
+
+        /// <summary>
+        /// Gets the mapped <see cref="NpgsqlTypes.NpgsqlDbType"/> value.
+        /// </summary>
+        public NpgsqlDbType NpgsqlDbType => (NpgsqlDbType)Value;
+    }
+}
