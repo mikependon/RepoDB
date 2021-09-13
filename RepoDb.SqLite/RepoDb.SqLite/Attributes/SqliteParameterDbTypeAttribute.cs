27d294df7e28da307a0fbdb0d920c20f92475044
Michael Pendon <michael.c.pendon@outlook.com>
2021-09-13 11:36:22 +0200

    #884 Added the new type map attribute for the Microsoft SqLite.

---
+++ RepoDb.SqLite/RepoDb.SqLite/Attributes/SqliteParameterDbTypeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+
+namespace RepoDb.Attributes
+{
+    /// <summary>
+    /// An attribute that is used to define a mapping of .NET CLR <see cref="Type"/> into its equivalent <see cref="SqliteType"/> value.
+    /// </summary>
+    public class SqliteParameterDbTypeAttribute : ParameterPropertyValueSetterAttribute
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="SqliteParameterDbTypeAttribute"/> class.
+        /// </summary>
+        /// <param name="value">A target <see cref="SqliteType"/> value.</param>
+        public SqliteParameterDbTypeAttribute(SqliteType value)
+            : base(typeof(SqliteParameter), nameof(SqliteParameter.SqliteType), value)
+        { }
+
+        /// <summary>
+        /// Gets a <see cref="SqliteType"/> that is currently mapped.
+        /// </summary>
+        public SqliteType SqliteType => (SqliteType)Value;
+    }
+}
