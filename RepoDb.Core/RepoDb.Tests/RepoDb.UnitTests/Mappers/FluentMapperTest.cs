a3276f18e93adb6adfbe682b03965a29acb0ea85
Michael Pendon <michael.c.pendon@outlook.com>
2021-09-23 13:22:31 +0200

    #899 Added the FluentMapper.PropertyHandlerAttributes() Unit Tests.

--- RepoDb.Core/RepoDb.Tests/RepoDb.UnitTests/Mappers/FluentMapperTest.cs
+++ RepoDb.Core/RepoDb.Tests/RepoDb.UnitTests/Mappers/FluentMapperTest.cs
@@ -2,20 +2,17 @@
 using RepoDb.Attributes;
 using RepoDb.Attributes.Parameter;
 using RepoDb.Exceptions;
+using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 
 namespace RepoDb.UnitTests.Mappers
 {
     [TestClass]
     public partial class FluentMapperTest
     {
-        [ClassInitialize]
-        public static void ClassInitialize(TestContext context)
-        {
-            Setup();
-        }
-
-        private static void Setup()
+        [TestInitialize()]
+        public void Initialize()
         {
             FluentMapper
                 .Entity<FluentMapperTestClass>()
@@ -23,7 +20,8 @@ namespace RepoDb.UnitTests.Mappers
                 .Primary(e => e.PrimaryId)
                 .Identity(e => e.PrimaryId)
                 .Column(e => e.PropertyString, "ColumnString")
-                .DbType(e => e.PropertyString, DbType.String);
+                .DbType(e => e.PropertyString, DbType.String)
+                .PropertyValueAttributes(e => e.PropertyString, GetPropertyValueAttributes());
 
             FluentMapper
                 .Entity<FluentMapperTestWithAttributesClass>()
@@ -31,7 +29,28 @@ namespace RepoDb.UnitTests.Mappers
                 .Primary(e => e.PrimaryId)
                 .Identity(e => e.PrimaryId)
                 .Column(e => e.PropertyString, "ColumnNVarChar")
-                .DbType(e => e.PropertyString, DbType.StringFixedLength);
+                .DbType(e => e.PropertyString, DbType.StringFixedLength)
+                .PropertyValueAttributes(e => e.PropertyString, GetPropertyValueAttributes());
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            ClassMapper.Clear();
+            PropertyMapper.Clear();
+            PrimaryMapper.Clear();
+            IdentityMapper.Clear();
+            TypeMapper.Clear();
+            //PropertyHandlerMapper.Clear();
+            PropertyValueAttributeMapper.Clear();
+
+            ClassMappedNameCache.Flush();
+            PropertyCache.Flush();
+            PrimaryCache.Flush();
+            IdentityCache.Flush();
+            TypeMapCache.Flush();
+            //PropertyHandlerMapper.Clear();
+            PropertyValueAttributeCache.Flush();
         }
 
         #region SubClasses
@@ -52,8 +71,49 @@ namespace RepoDb.UnitTests.Mappers
             public int PrimaryId { get; set; }
 
             [Map("ColumnString"), TypeMap(DbType.String)]
+            [
+                Name("ColumnDecimal"),
+                DbType(DbType.Decimal),
+                Direction(ParameterDirection.InputOutput),
+                IsNullable(true),
+                Precision(100),
+                Scale(2),
+                Size(256)
+            ]
             public string PropertyString { get; set; }
         }
+
+        #endregion
+
+        #region Helpers
+
+        private IEnumerable<PropertyValueAttribute> GetPropertyValueAttributes() =>
+            new PropertyValueAttribute[]
+            {
+                // Different Values
+                new NameAttribute("ColumnString"),
+                new DbTypeAttribute(DbType.StringFixedLength),
+                new DirectionAttribute(ParameterDirection.ReturnValue),
+                new SizeAttribute(512),
+                // Same Values
+                new IsNullableAttribute(true),
+                new PrecisionAttribute(100),
+                new ScaleAttribute(2)
+            };
+
+        private IEnumerable<PropertyValueAttribute> GetPropertyValueAttributesForOverriding() =>
+            new PropertyValueAttribute[]
+            {
+                // Different Values
+                new NameAttribute("OverridingColumnString"),
+                new DbTypeAttribute(DbType.AnsiString),
+                new DirectionAttribute(ParameterDirection.InputOutput),
+                new SizeAttribute(1024),
+                // Same Values
+                new IsNullableAttribute(false),
+                new PrecisionAttribute(200),
+                new ScaleAttribute(4)
+            };
 
         #endregion
 
@@ -66,7 +126,7 @@ namespace RepoDb.UnitTests.Mappers
          */
 
         [TestMethod]
-        public void TestFluentMapMapping()
+        public void TestFluentMapTableMapping()
         {
             // Act
             var actual = ClassMappedNameCache.Get<FluentMapperTestClass>();
@@ -81,7 +141,7 @@ namespace RepoDb.UnitTests.Mappers
          */
 
         [TestMethod]
-        public void TestFluentMapMappingWithMapAttribute()
+        public void TestFluentMapTableMappingWithMapAttribute()
         {
             // Act
             var actual = ClassMappedNameCache.Get<FluentMapperTestWithAttributesClass>();
@@ -96,7 +156,7 @@ namespace RepoDb.UnitTests.Mappers
          */
 
         [TestMethod]
-        public void TestFluentMapMappingOverride()
+        public void TestFluentMapTableMappingOverride()
         {
             // Setup
             FluentMapper
@@ -116,7 +176,7 @@ namespace RepoDb.UnitTests.Mappers
          */
 
         [TestMethod, ExpectedException(typeof(MappingExistsException))]
-        public void ThrowExceptionOnFluentMappingThatIsAlreadyExisting()
+        public void ThrowExceptionOnFluentMapTableMappingThatIsAlreadyExisting()
         {
             // Setup
             FluentMapper
@@ -158,25 +218,25 @@ namespace RepoDb.UnitTests.Mappers
             Assert.AreEqual(expected, actual.GetMappedName());
         }
 
-        ///*
-        // * Override True
-        // */
+        /*
+         * Override True
+         */
 
-        //[TestMethod]
-        //public void TestFluentMapPrimaryMappingOverride()
-        //{
-        //    // Setup
-        //    FluentMapper
-        //        .Entity<FluentMapperTestClass>()
-        //        .Primary(e => e.RowId, true);
+        [TestMethod]
+        public void TestFluentMapPrimaryMappingOverride()
+        {
+            // Setup
+            FluentMapper
+                .Entity<FluentMapperTestClass>()
+                .Primary(e => e.RowId, true);
 
-        //    // Act
-        //    var actual = PrimaryCache.Get<FluentMapperTestClass>();
-        //    var expected = "RowId";
+            // Act
+            var actual = PrimaryCache.Get<FluentMapperTestClass>();
+            var expected = "RowId";
 
-        //    // Assert
-        //    Assert.AreEqual(expected, actual.GetMappedName());
-        //}
+            // Assert
+            Assert.AreEqual(expected, actual.GetMappedName());
+        }
 
         /*
          * Override False
@@ -225,25 +285,25 @@ namespace RepoDb.UnitTests.Mappers
             Assert.AreEqual(expected, actual.GetMappedName());
         }
 
-        ///*
-        // * Override True
-        // */
+        /*
+         * Override True
+         */
 
-        //[TestMethod]
-        //public void TestFluentMapIdentityMappingOverride()
-        //{
-        //    // Setup
-        //    FluentMapper
-        //        .Entity<FluentMapperTestClass>()
-        //        .Identity(e => e.RowId, true);
+        [TestMethod]
+        public void TestFluentMapIdentityMappingOverride()
+        {
+            // Setup
+            FluentMapper
+                .Entity<FluentMapperTestClass>()
+                .Identity(e => e.RowId, true);
 
-        //    // Act
-        //    var actual = IdentityCache.Get<FluentMapperTestClass>();
-        //    var expected = "RowId";
+            // Act
+            var actual = IdentityCache.Get<FluentMapperTestClass>();
+            var expected = "RowId";
 
-        //    // Assert
-        //    Assert.AreEqual(expected, actual.GetMappedName());
-        //}
+            // Assert
+            Assert.AreEqual(expected, actual.GetMappedName());
+        }
 
         /*
          * Override False
@@ -256,6 +316,73 @@ namespace RepoDb.UnitTests.Mappers
             FluentMapper
                 .Entity<FluentMapperTestClass>()
                 .Identity(e => e.RowId);
+        }
+
+        #endregion
+
+        #region Column
+
+        /* 
+         * No MapAttribute
+         */
+
+        [TestMethod]
+        public void TestFluentMapColumnMapping()
+        {
+            // Act
+            var actual = PropertyMappedNameCache.Get<FluentMapperTestClass>(e => e.PropertyString);
+            var expected = "ColumnString";
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /*
+         * With MapAttribute
+         */
+
+        [TestMethod]
+        public void TestFluentMapColumnMappingWithMapAttribute()
+        {
+            // Act
+            var actual = PropertyMappedNameCache.Get<FluentMapperTestWithAttributesClass>(e => e.PropertyString);
+            var expected = "ColumnString";
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /*
+         * Override True
+         */
+
+        [TestMethod]
+        public void TestFluentMapColumnMappingOverride()
+        {
+            // Setup
+            FluentMapper
+                .Entity<FluentMapperTestClass>()
+                .Column(e => e.PropertyString, "ColumnStringOverriden", true);
+
+            // Act
+            var actual = PropertyMapper.Get<FluentMapperTestClass>(e => e.PropertyString);
+            var expected = "ColumnStringOverriden";
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /*
+         * Override False
+         */
+
+        [TestMethod, ExpectedException(typeof(MappingExistsException))]
+        public void ThrowExceptionOnFluentMapColumnMappingThatIsAlreadyExisting()
+        {
+            // Setup
+            FluentMapper
+                .Entity<FluentMapperTestWithAttributesClass>()
+                .Column(e => e.PropertyString, "ColumnStringOverriden");
         }
 
         #endregion
@@ -292,25 +419,25 @@ namespace RepoDb.UnitTests.Mappers
             Assert.AreEqual(expected, actual);
         }
 
-        ///*
-        // * Override True
-        // */
+        /*
+         * Override True
+         */
 
-        //[TestMethod]
-        //public void TestFluentMapDbTypeMappingOverride()
-        //{
-        //    // Setup
-        //    FluentMapper
-        //        .Entity<FluentMapperTestClass>()
-        //        .DbType(e => e.PropertyString, DbType.AnsiString, true);
+        [TestMethod]
+        public void TestFluentMapDbTypeMappingOverride()
+        {
+            // Setup
+            FluentMapper
+                .Entity<FluentMapperTestClass>()
+                .DbType(e => e.PropertyString, DbType.AnsiString, true);
 
-        //    // Act
-        //    var actual = TypeMapCache.Get<FluentMapperTestClass>();
-        //    var expected = DbType.AnsiString;
+            // Act
+            var actual = TypeMapCache.Get<FluentMapperTestClass>(e => e.PropertyString);
+            var expected = DbType.AnsiString;
 
-        //    // Assert
-        //    Assert.AreEqual(expected, actual);
-        //}
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
 
         /*
          * Override False
@@ -323,6 +450,103 @@ namespace RepoDb.UnitTests.Mappers
             FluentMapper
                 .Entity<FluentMapperTestClass>()
                 .DbType(e => e.PropertyString, DbType.AnsiString);
+        }
+
+        #endregion
+
+        // PropertyHandler
+
+        #region PropertyValueAttribute
+
+        /* 
+         * No MapAttribute
+         */
+
+        [TestMethod]
+        public void TestFluentMapPropertyValueAttributesMapping()
+        {
+            // Act
+            var actual = PropertyValueAttributeCache
+                .Get<FluentMapperTestClass>(e => e.PropertyString, true);
+
+            // Assert
+            Assert.AreEqual(7, actual.Count());
+        }
+
+        [TestMethod]
+        public void TestFluentMapPropertyValueAttributesMappingWithIncludeMappingFalse()
+        {
+            // Act
+            var actual = PropertyValueAttributeCache
+                .Get<FluentMapperTestClass>(e => e.PropertyString, false);
+
+            // Assert
+            Assert.AreEqual(0, actual.Count());
+        }
+
+        /*
+         * With MapAttribute
+         */
+
+        [TestMethod]
+        public void TestFluentMapPropertyValueAttributesMappingWithMapAttribute()
+        {
+            // Act
+            var actual = PropertyValueAttributeCache
+                .Get<FluentMapperTestWithAttributesClass>(e => e.PropertyString, true);
+
+            // Assert
+            Assert.AreEqual(12, actual.Count());
+        }
+
+        [TestMethod]
+        public void TestFluentMapPropertyValueAttributesMappingWithMapAttributeWithIncludeMappingFalse()
+        {
+            // Act
+            var actual = PropertyValueAttributeCache
+                .Get<FluentMapperTestWithAttributesClass>(e => e.PropertyString, false);
+
+            // Assert
+            Assert.AreEqual(8, actual.Count());
+        }
+
+        /*
+         * Override True
+         */
+
+        [TestMethod]
+        public void TestFluentMapPropertyValueAttributesMappingOverride()
+        {
+            // Prepare
+            var attributes = GetPropertyValueAttributesForOverriding().ToList();
+
+            // Setup
+            FluentMapper
+                .Entity<FluentMapperTestClass>()
+                .PropertyValueAttributes(e => e.PropertyString, attributes, true);
+
+            // Act
+            var actual = PropertyValueAttributeCache
+                .Get<FluentMapperTestClass>(e => e.PropertyString, true).ToList();
+
+            // Assert
+            for (var i = 0; i < attributes.Count; i++)
+            {
+                Assert.AreEqual(attributes[i], actual[i]);
+            }
+        }
+
+        /*
+         * Override False
+         */
+
+        [TestMethod, ExpectedException(typeof(MappingExistsException))]
+        public void ThrowExceptionOnFluentMapPropertyValueAttributesMappingThatIsAlreadyExisting()
+        {
+            // Setup
+            FluentMapper
+                .Entity<FluentMapperTestWithAttributesClass>()
+                .PropertyValueAttributes(e => e.PropertyString, GetPropertyValueAttributes());
         }
 
         #endregion
