8ce02d8fe12859f76c6a5318498ed240a4ce6d0a
Michael Pendon <michael.c.pendon@outlook.com>
2021-10-01 23:14:28 +0200

    Update NpgsqlBulkInsertMapItem.cs

--- RepoDb.Extensions/RepoDb.PostgreSql.BulkOperations/RepoDb.PostgreSql.BulkOperations/NpgsqlBulkInsertMapItem.cs
+++ RepoDb.Extensions/RepoDb.PostgreSql.BulkOperations/RepoDb.PostgreSql.BulkOperations/NpgsqlBulkInsertMapItem.cs
@@ -9,19 +9,21 @@ namespace RepoDb.PostgreSql.BulkOperations
     /// </summary>
     public class NpgsqlBulkInsertMapItem : BulkInsertMapItem
     {
+        private static readonly ClientTypeToNpgsqlDbTypeResolver clientTypeToNpgsqlDbTypeResolver = new ClientTypeToNpgsqlDbTypeResolver();
+
         /// <summary>
         /// Creates a new instance of <see cref="BulkInsertMapItem"/> object.
         /// </summary>
         /// <param name="sourceColumn">The name of the source column or property. This respects the mapping of the properties if the source type is an entity model.</param>
         /// <param name="destinationColumn">The name of the destination column in the database.</param>
         /// <param name="type">
-        /// The type to be identified for <see cref="NpgsqlTypes.NpgsqlDbType"/>. The <see cref="ClientTypeToNpgsqlDbTypeResolver"/> object
+        /// The .NET CLR type to be used to identify the equivalent <see cref="NpgsqlTypes.NpgsqlDbType"/> value. The <see cref="ClientTypeToNpgsqlDbTypeResolver"/> object
         /// is used for identification.
         /// </param>
         public NpgsqlBulkInsertMapItem(string sourceColumn,
             string destinationColumn,
             Type type) :
-            this(sourceColumn, destinationColumn, new ClientTypeToNpgsqlDbTypeResolver().Resolve(type))
+            this(sourceColumn, destinationColumn, clientTypeToNpgsqlDbTypeResolver.Resolve(type))
         { }
 
         /// <summary>
@@ -39,7 +41,7 @@ namespace RepoDb.PostgreSql.BulkOperations
         }
 
         /// <summary>
-        /// Gets the <see cref="NpgsqlDbType"/> type value being used when writing.
+        /// Gets the <see cref="NpgsqlTypes.NpgsqlDbType"/> type value being used when writing.
         /// </summary>
         public NpgsqlDbType? NpgsqlDbType { get; }
     }
