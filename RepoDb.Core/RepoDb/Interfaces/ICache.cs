976db86e50a76990124a269cc1b523f57b5b583a
Michael Camara Pendon <michael.c.pendon@outlook.com>
2022-09-09 21:52:59 +0200

    #1080 Initial changes to support the 'Async' methods.

--- RepoDb.Core/RepoDb/Interfaces/ICache.cs
+++ RepoDb.Core/RepoDb/Interfaces/ICache.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace RepoDb.Interfaces
 {
@@ -7,6 +9,8 @@ namespace RepoDb.Interfaces
     /// </summary>
     public interface ICache : IEnumerable
     {
+        #region Sync
+
         /// <summary>
         /// Adds a cache item value.
         /// </summary>
@@ -58,5 +62,74 @@ namespace RepoDb.Interfaces
         /// <param name="throwException">Throws an exception if the operation has failed to remove an item.</param>
         void Remove(string key,
             bool throwException = true);
+
+        #endregion
+
+        #region Async
+
+        /// <summary>
+        /// Adds a cache item value in an asynchronous way.
+        /// </summary>
+        /// <typeparam name="T">The type of the cache item value.</typeparam>
+        /// <param name="key">The key to the cache.</param>
+        /// <param name="value">The value of the cache.</param>
+        /// <param name="expiration">The expiration in minutes of the cache item.</param>
+        /// <param name="throwException">Throws an exception if the operation has failed to add an item.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> object to be used during the asynchronous operation.</param>
+        Task AddAsync<T>(string key,
+            T value,
+            int expiration = Constant.DefaultCacheItemExpirationInMinutes,
+            bool throwException = true,
+            CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Adds a cache item value in an asynchronous way.
+        /// </summary>
+        /// <typeparam name="T">The type of the cache item value.</typeparam>
+        /// <param name="item">The cache item to be added in the collection.</param>
+        /// <param name="throwException">Throws an exception if the operation has failed to add an item.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> object to be used during the asynchronous operation.</param>
+        Task AddAsync<T>(CacheItem<T> item,
+            bool throwException = true,
+            CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Clears the collection of the cache in an asynchronous way.
+        /// </summary>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> object to be used during the asynchronous operation.</param>
+        Task ClearAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Checks whether the key is present in the collection in an asynchronous way.
+        /// </summary>
+        /// <param name="key">The name of the key to be checked.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> object to be used during the asynchronous operation.</param>
+        /// <returns>A boolean value that signifies the presence of the key from the collection.</returns>
+        Task<bool> ContainsAsync(string key,
+            CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Gets an object from the cache collection in an asynchronous way.
+        /// </summary>
+        /// <typeparam name="T">The type of the cache item value.</typeparam>
+        /// <param name="key">The key of the cache object to be retrieved.</param>
+        /// <param name="throwException">Throws an exception if the item is not found.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> object to be used during the asynchronous operation.</param>
+        /// <returns>A cached item object from the cache collection based on the given key.</returns>
+        Task<CacheItem<T>> GetAsync<T>(string key,
+            bool throwException = true,
+            CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Removes the item from the cache collection in an asynchronous way.
+        /// </summary>
+        /// <param name="key">The key of the item to be removed from the cache collection.</param>
+        /// <param name="throwException">Throws an exception if the operation has failed to remove an item.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> object to be used during the asynchronous operation.</param>
+        Task RemoveAsync(string key,
+            bool throwException = true,
+            CancellationToken cancellationToken = default);
+
+        #endregion
     }
 }
