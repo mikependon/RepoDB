9581c88a404b30aed352e59b553f6ca90deff8fa
sergergood <serzher@inbox.ru>
2022-12-08 21:22:04 +0300

    comments added

--- RepoDb.Core/RepoDb/Cachers/TypeCache.cs
+++ RepoDb.Core/RepoDb/Cachers/TypeCache.cs
@@ -3,11 +3,19 @@ using System.Collections.Concurrent;
 
 namespace RepoDb;
 
+/// <summary>
+/// A class that is being used to cache the type.
+/// </summary>
 public static class TypeCache
 {
     private static readonly ConcurrentDictionary<Type, CachedType> cache = new();
     private static readonly CachedType nullCachedType = new(null);
 
+    /// <summary>
+    /// Gets the cached <see cref="CachedType"/> object that is being mapped on a type.
+    /// </summary>
+    /// <param name="type">The target type.</param>
+    /// <returns>The mapped <see cref="CachedType"/> object of the target type.</returns>
     public static CachedType Get(Type type)
     {
         if (type is null)
--- RepoDb.Core/RepoDb/Cachers/Types/CachedType.cs
+++ RepoDb.Core/RepoDb/Cachers/Types/CachedType.cs
@@ -4,6 +4,9 @@ using RepoDb.Extensions;
 
 namespace RepoDb;
 
+/// <summary>
+/// A class holds the type with lazy properties.
+/// </summary>
 public class CachedType
 {
     private readonly Lazy<PropertyInfo[]> lazyGetProperties;
@@ -13,6 +16,10 @@ public class CachedType
     private readonly Lazy<bool> lazyIsDictionaryStringObject;
     private readonly Lazy<bool> lazyIsNullable;
 
+    /// <summary>
+    /// Creates a new instance of <see cref="CachedType" /> object.
+    /// </summary>
+    /// <param name="type">The target type.</param>
     public CachedType(Type type)
     {
         lazyGetUnderlyingType = new Lazy<Type>(() => type?.GetUnderlyingType());
@@ -26,15 +33,42 @@ public class CachedType
         lazyIsNullable = new Lazy<bool>(type.IsNullable);
     }
 
+    /// <summary>
+    /// Returns all the public properties of the current Type.
+    /// </summary>
+    /// <returns>
+    /// An array of PropertyInfo objects representing all public properties of the current Type.
+    /// -or- An empty array of type PropertyInfo, if the current Type does not have public properties.
+    /// </returns>
     public PropertyInfo[] GetProperties() => lazyGetProperties.Value;
 
+    /// <summary>
+    /// Returns the underlying type of the current type. If there is no underlying type, this will return the current type.
+    /// </summary>
+    /// <returns>The underlying type or the current type.</returns>
     public Type GetUnderlyingType() => lazyGetUnderlyingType.Value;
 
+    /// <summary>
+    /// Checks whether the current type is an anonymous type.
+    /// </summary>
+    /// <returns>Returns true if the current type is an anonymous class.</returns>
     public bool IsAnonymousType() => lazyIsAnonymousType.Value;
 
+    /// <summary>
+    /// Checks whether the current type is a class.
+    /// </summary>
+    /// <returns>Returns true if the current type is a class.</returns>
     public bool IsClassType() => lazyIsClassType.Value;
 
+    /// <summary>
+    /// Checks whether the current type is of type <see cref="IDictionary{TKey, TValue}"/> (with string/object key-value-pair).
+    /// </summary>
+    /// <returns>Returns true if the current type is of type <see cref="IDictionary{TKey, TValue}"/> (with string/object key-value-pair).</returns>
     public bool IsDictionaryStringObject() => lazyIsDictionaryStringObject.Value;
 
+    /// <summary>
+    /// Checks whether the current type is wrapped within a <see cref="Nullable{T}"/> object.
+    /// </summary>
+    /// <returns>Returns true if the current type is wrapped within a <see cref="Nullable{T}"/> object.</returns>
     public bool IsNullable() => lazyIsNullable.Value;
 }
