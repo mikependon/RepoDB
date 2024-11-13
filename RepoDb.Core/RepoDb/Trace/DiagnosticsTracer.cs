f90f64acd196222b393a20744ea66a77adb91c08
Bert Huijben <bert@qqmail.nl>
2024-11-13 09:30:45 +0100

    Add default tracer

---
+++ RepoDb.Core/RepoDb/Trace/DiagnosticsTracer.cs
@@ -0,0 +1,61 @@
+﻿#nullable enable
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using RepoDb.Interfaces;
+using SystemDiagnosticsTrace = System.Diagnostics.Trace;
+
+namespace RepoDb.Trace
+{
+    /// <summary>
+    /// Creates a tracer that writes output to <see cref="SystemDiagnosticsTrace"/>
+    /// </summary>
+    public sealed class DiagnosticsTracer : ITrace
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="log"></param>
+        public void AfterExecution<TResult>(ResultTraceLog<TResult> log)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="log"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task AfterExecutionAsync<TResult>(ResultTraceLog<TResult> log, CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="log"></param>
+        public void BeforeExecution(CancellableTraceLog log)
+        {
+            SystemDiagnosticsTrace.WriteLine(log);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="log"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public Task BeforeExecutionAsync(CancellableTraceLog log, CancellationToken cancellationToken = default)
+        {
+            SystemDiagnosticsTrace.WriteLine(log);
+
+            return Task.CompletedTask;
+        }
+    }
+}
