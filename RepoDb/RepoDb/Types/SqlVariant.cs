bc1e499b7c1726611aae3368ba5467692feaa205
Bert Huijben <bert@qqmail.nl>
2024-11-13 12:41:30 +0100

    Remove dead code

--- RepoDb/RepoDb/Types/SqlVariant.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data;
-
-namespace RepoDb.Types
-{
-    /// <summary>
-    /// A type holder for sql_variant <see cref="DbType"/> value.
-    /// </summary>
-    public class SqlVariant { }
-}
