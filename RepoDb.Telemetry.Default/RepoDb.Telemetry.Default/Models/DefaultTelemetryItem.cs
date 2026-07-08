0ab3ddced8b225706e37de5a02de60c21e877c08
Michael Camara Pendon <michael.c.pendon@outlook.com>
2026-07-08 22:35:43 +0200

    Added the resources to the Telemetry projects (Nuget README, icons, etc).

--- RepoDb.Telemetry.Default/RepoDb.Telemetry.Default/Models/DefaultTelemetryItem.cs
+++ RepoDb.Telemetry.Default/RepoDb.Telemetry.Default/Models/DefaultTelemetryItem.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using RepoDb.Telemetry.Core;
+﻿using RepoDb.Telemetry.Core;
 
 namespace RepoDb.Telemetry.Default
 {
     /// <summary>
-    /// 
+    /// A class that is used to define the default telemetry data to be published to the insights solution.
     /// </summary>
     public class DefaultTelemetryItem : TelemetryItem
     {
