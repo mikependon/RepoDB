288f85d1eacfc48f89c55f69f99f3e1381847641
Mauro Cardillo <mauro.cardillo@gmail.com>
2026-08-23 18:04:34 +0200

    Add MSTestSettings.cs for integration tests configuration
    Disable test parallelization

---
+++ RepoDb.ClickHouse/RepoDb.ClickHouse.IntegrationTests/MSTestSettings.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[assembly: DoNotParallelize()]
+
+namespace RepoDb.ClickHouse.IntegrationTests;
