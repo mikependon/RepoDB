b37af1991bf042a33e39709d6bb14553405413fb
Paolo Bassi <paolo.bassi@doctech.nu>
2026-08-26 12:41:47 +0200

    Add BoolPropertyHandler for SByte-bool conversion
    Introduced BoolPropertyHandler in RepoDb.MySqlConnector.PropertyHandlers. Implements IPropertyHandler<SByte, bool> to convert SByte to bool (non-zero is true) and bool to SByte (true is 1, false is 0).

---
+++ RepoDb.MySqlConnector/RepoDb.MySqlConnector/PropertyHandlers/BoolPropertyHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using RepoDb.Interfaces;
+using RepoDb.Options;
+
+namespace RepoDb.MySqlConnector.PropertyHandlers
+{
+    public class BoolPropertyHandler : IPropertyHandler<SByte, bool>
+    {
+        public bool Get(SByte input, PropertyHandlerGetOptions options)
+        {
+            return input != 0;
+        }
+
+        public SByte Set(bool input, PropertyHandlerSetOptions options)
+        {
+            return input ? (SByte)1 : (SByte)0;
+        }
+    }
+}
