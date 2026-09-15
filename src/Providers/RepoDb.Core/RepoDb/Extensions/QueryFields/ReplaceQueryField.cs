ffc4449b686bf8eeffbd83ae9518a2e31c9bc8a0
Paolo Bassi <paolo.bassi@doctech.nu>
2026-09-15 11:39:07 +0200

    Add ReplaceQueryField for SQL REPLACE in queries
    Introduced ReplaceQueryField in RepoDb.Extensions.QueryFields, inheriting from FunctionalQueryField. Enables creation of query fields using the SQL REPLACE function, supporting field name, operation, value, optional DbType, search, and replacement values. Facilitates string search and replace in queries.

---
+++ src/Providers/RepoDb.Core/RepoDb/Extensions/QueryFields/ReplaceQueryField.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using RepoDb.Enumerations;
+
+namespace RepoDb.Extensions.QueryFields
+{
+    public class ReplaceQueryField : FunctionalQueryField
+    {
+        /// <summary>
+        /// A functional-based <see cref="QueryField"/> object that is using the REPLACE function.
+        /// </summary>
+        /// <param name="fieldName">The name of the field for the query expression.</param>
+        /// <param name="operation">The operation to be used for the query expression.</param>
+        /// <param name="value">The value to be used for the query expression.</param>
+        /// <param name="searchValue">The value to be searched for in the query expression.</param>
+        /// <param name="replaceValue">The value to replace the search value with in the query expression.</param>
+        public ReplaceQueryField(
+        string fieldName,
+        Operation operation,
+        object value,
+        string searchValue,
+        string replaceValue)
+        : this(fieldName, operation, value, null, searchValue, replaceValue)
+        { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReplaceQueryField"/> class.
+        /// </summary>
+        /// <param name="fieldName">The name of the field for the query expression.</param>
+        /// <param name="operation">The operation to be used for the query expression.</param>
+        /// <param name="value">The value to be used for the query expression.</param>
+        /// <param name="dbType">The database type of the value.</param>
+        /// <param name="searchValue">The value to be searched for in the query expression.</param>
+        /// <param name="replaceValue">The value to replace the search value with in the query expression.</param>
+        public ReplaceQueryField(
+            string fieldName,
+            Operation operation,
+            object value,
+            DbType? dbType,
+            string searchValue,
+            string replaceValue)
+            : base(fieldName, operation, value, dbType, $"REPLACE({{0}}, '{searchValue}', '{replaceValue}')")
+        { }
+    }
+}
